washingtondc-emu/washingtondc

commit 0553a61c409ff0e8e424a9580516d399de84f1dd

unit_tests: bring sh4inst_test up to speed with sh4asm refactoring in 12cf424bb8e3a01d2a2c50fda95968fb99f7e851
This fixes the test failures reported recently by travis-ci

src/Dreamcast.cpp
@@ -40,7 +40,7 @@ Dreamcast::Dreamcast(char const *bios_path) {
 #endif
 
     BiosFile bios(bios_path);
-    mem->load_program<inst_t*>(0, (inst_t*)bios.begin(), (inst_t*)bios.end());
+    mem->load_binary<inst_t, inst_t*>(0, (inst_t*)bios.begin(), (inst_t*)bios.end());
 }
 
 Dreamcast::~Dreamcast() {

src/hw/sh4/Memory.hpp
@@ -47,20 +47,20 @@ public:
      * loads a program into the given address.  the InputIterator's
      * indirect method (overload*) should return an inst_t.
      */
-    template<class InputIterator>
-    void load_program(addr32_t where, InputIterator start, InputIterator end);
+    template<typename data_tp, class InputIterator>
+    void load_binary(addr32_t where, InputIterator start, InputIterator end);
 
 private:
     size_t size;
     boost::uint8_t *mem;
 };
 
-template<class InputIterator>
-void Memory::load_program(addr32_t where, InputIterator start,
-			  InputIterator end) {
-    for (InputIterator it = start; it != end; it++, where += 2) {
-	inst_t tmp = (inst_t)*it;
-	write(&tmp, where, sizeof(tmp));
+template<typename data_tp, class InputIterator>
+void Memory::load_binary(addr32_t where, InputIterator start,
+                          InputIterator end) {
+    for (InputIterator it = start; it != end; it++, where++) {
+        data_tp tmp = *it;
+        write(&tmp, where, sizeof(tmp));
     }
 }
 

commit e5430162589bf9cdb4ed389a02a0c38c0a81211c

Fix bios-loading bug introduced in 0553a61c409ff0e8e424a9580516d399de84f1dd
I have no idea why I casted those pointers to inst_t.

src/Dreamcast.cpp
@@ -40,7 +40,7 @@ Dreamcast::Dreamcast(char const *bios_path) {
 #endif
 
     BiosFile bios(bios_path);
-    mem->load_binary<inst_t, inst_t*>(0, (inst_t*)bios.begin(), (inst_t*)bios.end());
+    mem->load_binary<uint8_t, uint8_t*>(0, bios.begin(), bios.end());
 }
 
 Dreamcast::~Dreamcast() {