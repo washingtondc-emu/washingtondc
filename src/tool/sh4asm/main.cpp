4de0d03c6b8a4defbf922f8b8111d70542ad3c49
snickerbockers <chimerasaurusrex@gmail.com>
2017-01-07 16:16:10 -0800

    sh4asm: handle exceptions and print them with boost::diagnostic_information

--- src/tool/sh4asm/main.cpp
+++ src/tool/sh4asm/main.cpp
@@ -51,6 +51,13 @@ static unsigned to_hex(char ch) {
     BOOST_THROW_EXCEPTION(InvalidParamError("character is not hex"));
 }
 
+struct options {
+    char const *filename_in, *filename_out;
+    bool bin_mode;
+    bool print_addrs;
+    bool disas;
+};
+
 static void print_usage(char const *cmd) {
     std::cerr << "Usage: " << cmd << " [-i input] [-o output] " <<
         "instruction" << std::endl;
@@ -62,13 +69,153 @@ static void print_addr(std::ostream *output, addr32_t addr) {
         std::setw(8) << addr << ":    ";
 }
 
+void do_disasm(std::istream *input, std::ostream *output,
+               struct options const *options) {
+    Sh4Prog::ByteList bin_dat;
+    uint8_t dat = 0;
+    bool even = true;
+    addr32_t pc = 0;
+
+    if (options->bin_mode) {
+        while (input->good()) {
+            input->read((char*)&dat, 1);
+            if (!input->fail())
+                bin_dat.push_back(dat);
+
+            if (bin_dat.size() >= 2) {
+                size_t idx = 0;
+                do {
+                    if (options->print_addrs)
+                        print_addr(output, pc);
+
+                    size_t old_idx = idx;
+                    (*output) << Sh4Prog::disassemble_single(bin_dat, &idx);
+                    size_t adv = idx - old_idx;
+                    pc += adv;
+                } while (idx < bin_dat.size());
+
+                bin_dat.clear();
+            }
+        }
+
+        if (bin_dat.size()) {
+            size_t idx = 0;
+            do {
+                if (options->print_addrs)
+                    print_addr(output, pc);
+
+                size_t old_idx = idx;
+                (*output) << Sh4Prog::disassemble_single(bin_dat, &idx);
+                size_t adv = idx - old_idx;
+                pc += adv;
+            } while (idx < bin_dat.size());
+        }
+    } else {
+        while (input->good()) {
+            char c = input->get();
+
+            if (c == std::char_traits<char>::eof())
+                break;
+
+            if (isspace(c)) {
+                if (!even)
+                    bin_dat.push_back(dat);
+                even = true;
+                continue;
+            }
+
+            if (even) {
+                dat = to_hex(c);
+            } else {
+                dat = (dat << 4) | to_hex(c);
+                bin_dat.push_back(dat);
+            }
+
+            even = !even;
+
+            if (bin_dat.size() >= 2) {
+                size_t idx = 0;
+                do {
+                    if (options->print_addrs)
+                        print_addr(output, pc);
+
+                    size_t old_idx = idx;
+                    (*output) << Sh4Prog::disassemble_single(bin_dat, &idx);
+                    size_t adv = idx - old_idx;
+                    pc += adv;
+                } while (idx < bin_dat.size());
+
+                bin_dat.clear();
+            }
+        }
+
+        if (!even)
+            bin_dat.push_back(dat);
+
+        if (bin_dat.size()) {
+            size_t idx = 0;
+            do {
+                if (options->print_addrs)
+                    print_addr(output, pc);
+
+                size_t old_idx = idx;
+                (*output) << Sh4Prog::disassemble_single(bin_dat, &idx);
+                size_t adv = idx - old_idx;
+                pc += adv;
+            } while (idx < bin_dat.size());
+        }
+    }
+}
+
+void do_asm(std::istream *input, std::ostream *output,
+            struct options const *options) {
+    while (input->good()) {
+        std::string line;
+        std::getline(*input, line, '\n');
+
+        /*
+         * filter out addresses left by the -l option in disassembler mode
+         * The way this is implemented effectively turns everything before
+         * the colon into a comment (because we ignore it), but users
+         * should not actually use rely on this behavior because eventually
+         * it may change.  Colons will probably be used as a suffix for
+         * labels at some point since that seems to be the standard way to
+         * implement labels across most assemblers.
+         */
+        size_t colon_idx = line.find_first_of(':');
+        if (colon_idx != std::string::npos)
+            line = line.substr(colon_idx + 1);
+
+        // trim leading whitespace
+        size_t first_non_whitespace_idx = line.find_first_not_of(" \t");
+        if (first_non_whitespace_idx != std::string::npos)
+            line = line.substr(first_non_whitespace_idx);
+
+        if (!only_whitespace(line))
+            line += "\n";
+
+        if (line.size()) {
+            Sh4Prog::ByteList bin_dat = Sh4Prog::assemble_single_line(line);
+            for (Sh4Prog::ByteList::iterator it = bin_dat.begin();
+                 it != bin_dat.end(); it++) {
+                if (options->bin_mode) {
+                    char dat = *it;
+                    output->write(&dat, 1);
+                } else {
+                    (*output) << std::hex << unsigned(*it) << std::endl;
+                }
+            }
+        }
+    }
+}
+
 int main(int argc, char **argv) {
+    int err_code = 0;
     int opt;
-    bool disas = false;
     char const *cmd = argv[0];
-    char const *filename_in = NULL, *filename_out = NULL;
-    bool bin_mode = false;
-    bool print_addrs = false;
+    struct options options;
+
+    memset(&options, 0, sizeof(options));
 
     std::ostream *output = &std::cout;
     std::istream *input = &std::cin;
@@ -78,19 +225,19 @@ int main(int argc, char **argv) {
     while ((opt = getopt(argc, argv, "bdli:o:")) != -1) {
         switch (opt) {
         case 'b':
-            bin_mode = true;
+            options.bin_mode = true;
             break;
         case 'd':
-            disas = true;
+            options.disas = true;
             break;
         case 'l':
-            print_addrs = true;
+            options.print_addrs = true;
             break;
         case 'i':
-            filename_in = optarg;
+            options.filename_in = optarg;
             break;
         case 'o':
-            filename_out = optarg;
+            options.filename_out = optarg;
             break;
         default:
             print_usage(cmd);
@@ -106,147 +253,22 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-    if (filename_in)
-        input = file_in = new std::ifstream(filename_in);
+    if (options.filename_in)
+        input = file_in = new std::ifstream(options.filename_in);
 
-    if (filename_out) {
-        output = file_out = new std::ofstream(filename_out);
+    if (options.filename_out) {
+        output = file_out = new std::ofstream(options.filename_out);
     }
 
-    if (disas) {
-        Sh4Prog::ByteList bin_dat;
-        uint8_t dat = 0;
-        bool even = true;
-        addr32_t pc = 0;
-
-        if (bin_mode) {
-            while (input->good()) {
-                input->read((char*)&dat, 1);
-                if (!input->fail())
-                    bin_dat.push_back(dat);
-
-                if (bin_dat.size() >= 2) {
-                    size_t idx = 0;
-                    do {
-                        if (print_addrs)
-                            print_addr(output, pc);
-
-                        size_t old_idx = idx;
-                        (*output) << Sh4Prog::disassemble_single(bin_dat, &idx);
-                        size_t adv = idx - old_idx;
-                        pc += adv;
-                    } while (idx < bin_dat.size());
-
-                    bin_dat.clear();
-                }
-            }
-
-            if (bin_dat.size()) {
-                size_t idx = 0;
-                do {
-                    if (print_addrs)
-                        print_addr(output, pc);
-
-                    size_t old_idx = idx;
-                    (*output) << Sh4Prog::disassemble_single(bin_dat, &idx);
-                    size_t adv = idx - old_idx;
-                    pc += adv;
-                } while (idx < bin_dat.size());
-            }
+    try {
+        if (options.disas) {
+            do_disasm(input, output, &options);
         } else {
-            while (input->good()) {
-                char c = input->get();
-
-                if (c == std::char_traits<char>::eof())
-                    break;
-
-                if (isspace(c)) {
-                    if (!even)
-                        bin_dat.push_back(dat);
-                    even = true;
-                    continue;
-                }
-
-                if (even) {
-                    dat = to_hex(c);
-                } else {
-                    dat = (dat << 4) | to_hex(c);
-                    bin_dat.push_back(dat);
-                }
-
-                even = !even;
-
-                if (bin_dat.size() >= 2) {
-                    size_t idx = 0;
-                    do {
-                        if (print_addrs)
-                            print_addr(output, pc);
-
-                        size_t old_idx = idx;
-                        (*output) << Sh4Prog::disassemble_single(bin_dat, &idx);
-                        size_t adv = idx - old_idx;
-                        pc += adv;
-                    } while (idx < bin_dat.size());
-
-                    bin_dat.clear();
-                }
-            }
-
-            if (!even)
-                bin_dat.push_back(dat);
-
-            if (bin_dat.size()) {
-                size_t idx = 0;
-                do {
-                    if (print_addrs)
-                        print_addr(output, pc);
-
-                    size_t old_idx = idx;
-                    (*output) << Sh4Prog::disassemble_single(bin_dat, &idx);
-                    size_t adv = idx - old_idx;
-                    pc += adv;
-                } while (idx < bin_dat.size());
-            }
+            do_asm(input, output, &options);
         }
-    } else {
-        while (input->good()) {
-            std::string line;
-            std::getline(*input, line, '\n');
-
-            /*
-             * filter out addresses left by the -l option in disassembler mode
-             * The way this is implemented effectively turns everything before
-             * the colon into a comment (because we ignore it), but users
-             * should not actually use rely on this behavior because eventually
-             * it may change.  Colons will probably be used as a suffix for
-             * labels at some point since that seems to be the standard way to
-             * implement labels across most assemblers.
-             */
-            size_t colon_idx = line.find_first_of(':');
-            if (colon_idx != std::string::npos)
-                line = line.substr(colon_idx + 1);
-
-            // trim leading whitespace
-            size_t first_non_whitespace_idx = line.find_first_not_of(" \t");
-            if (first_non_whitespace_idx != std::string::npos)
-                line = line.substr(first_non_whitespace_idx);
-
-            if (!only_whitespace(line))
-                line += "\n";
-
-            if (line.size()) {
-                Sh4Prog::ByteList bin_dat = Sh4Prog::assemble_single_line(line);
-                for (Sh4Prog::ByteList::iterator it = bin_dat.begin();
-                     it != bin_dat.end(); it++) {
-                    if (bin_mode) {
-                        char dat = *it;
-                        output->write(&dat, 1);
-                    } else {
-                        (*output) << std::hex << unsigned(*it) << std::endl;
-                    }
-                }
-            }
-        }
+    } catch (BaseException& exc) {
+        std::cerr << boost::diagnostic_information(exc);
+        err_code = 1;
     }
 
     if (file_in)
@@ -254,5 +276,5 @@ int main(int argc, char **argv) {
     if (file_out)
         delete file_out;
 
-    return 0;
+    return err_code;
 }
