aaef5aaf097f388aaa261599930c23d07861f8b5
snickerbockers <chimerasaurusrex@gmail.com>
2017-02-26 10:13:14 -0800

    Add missing files
    these should have been added in e6958e95a898292aa9e1ee283cdd664594cc362c

---
+++ src/hw/pvr2/pvr2_tex_mem.cpp
@@ -0,0 +1,54 @@
+/*******************************************************************************
+ *
+ *
+ *    WashingtonDC Dreamcast Emulator
+ *    Copyright (C) 2017 snickerbockers
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ ******************************************************************************/
+
+#include "MemoryMap.hpp"
+#include "BaseException.hpp"
+
+static uint8_t pvr2_tex_mem[ADDR_TEX_LAST - ADDR_TEX_FIRST + 1];
+
+int pvr2_tex_mem_read(void *buf, size_t addr, size_t len) {
+    void const *start_addr = pvr2_tex_mem + (addr - ADDR_TEX_FIRST);
+
+    if (addr < ADDR_TEX_FIRST || addr > ADDR_TEX_LAST ||
+        ((addr - 1 + len) > ADDR_TEX_LAST) ||
+        ((addr - 1 + len) < ADDR_TEX_FIRST)) {
+        BOOST_THROW_EXCEPTION(UnimplementedError() <<
+                              errinfo_feature("aw fuck it"));
+    }
+
+    memcpy(buf, start_addr, len);
+    return 0;
+}
+
+int pvr2_tex_mem_write(void const *buf, size_t addr, size_t len) {
+    void *start_addr = pvr2_tex_mem + (addr - ADDR_TEX_FIRST);
+
+    if (addr < ADDR_TEX_FIRST || addr > ADDR_TEX_LAST ||
+        ((addr - 1 + len) > ADDR_TEX_LAST) ||
+        ((addr - 1 + len) < ADDR_TEX_FIRST)) {
+        BOOST_THROW_EXCEPTION(UnimplementedError() <<
+                              errinfo_feature("aw fuck it"));
+    }
+
+    memcpy(start_addr, buf, len);
+    return 0;
+}
