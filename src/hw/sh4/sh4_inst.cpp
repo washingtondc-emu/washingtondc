df44c76da622f54a1cb2e3fa12864b211f8186ed
snickerbockers <chimerasaurusrex@gmail.com>
2017-03-08 01:28:26 -0800

    sh4 interpreter: fix problem with RTE not restoring the SR register from SSR properly
    holy shit, I can't believe I actually made that mistake.  And here I thought
    that RTE was so simple that it didn't need a unit test...

--- src/hw/sh4/sh4_inst.cpp
+++ src/hw/sh4/sh4_inst.cpp
@@ -1191,7 +1191,7 @@ void sh4_inst_rte(Sh4 *sh4, Sh4OpArgs inst) {
      * that needs this to work right.
      */
     sh4->delayed_branch_addr = sh4->reg[SH4_REG_SPC];
-    sh4->reg[SH4_REG_SR] = SH4_REG_SSR;
+    sh4->reg[SH4_REG_SR] = sh4->reg[SH4_REG_SSR];
 
     sh4_next_inst(sh4);
 }
