dd1ff2a12fadb0f04d302aa5ee7f5450c9b09f16
snickerbockers <chimerasaurusrex@gmail.com>
2017-03-27 08:05:08 -0700

    AICA: Add workaround to get the RTC sort-of working
    I hardcoded it to approximately Jan 1, 2000 at 12:00 AM.  It never
    increments because I'd like to hold off on implementing AICA for real
    until after I've written a real scheduler for the CPU.
    
    The reason I'm doing this is so that I can play the BSD cave adventure game
    from KallistiOS' examples directory, and it translates time to it's own epoch
    which is based at 1977.
    
    If the RTC is set prior to that, it'll break the game's logic and refuse to
    start.  The reason for this is that there's a snarky joke where it tells you
    that you need to be more patient if you save your game and then immediately
    load that saved game, and the logic for implementing that will return a false
    positive if the current time is negative.

--- src/hw/aica/aica_rtc.cpp
+++ src/hw/aica/aica_rtc.cpp
@@ -20,20 +20,53 @@
  *
  ******************************************************************************/
 
+#include <iostream>
+
+#include <boost/cstdint.hpp>
+
 #include "MemoryMap.hpp"
 
 #include "aica_rtc.hpp"
+
+/*
+ * HACK
+ * We hardcode the RTC value for now and never increment it.
+ *
+ * this should be approximately january 1 2000 at 12:00 AM.  I say it's
+ * approximate because I didn't take leap years or leap seconds
+ * into account.  The value of 631152000 comes from KallistiOS and represents
+ * the offset between the Dreamcast epoch and the Unix epoch.
+ *
+ * The Dreamcast epoch is January 1, 1950 at 12:00 AM for some reason.
+ * This means that the Sega Dreamcast will experience it's own version of
+ * Unix's infamous 2038 problem in 2018.  It's a good thing the RTC battery
+ * isn't reliable enough to last that long.
+ */
+static const uint32_t RTC_DEFAULT = 631152000 + 365 * 24 * 60 * 60 * 30;
 
 static uint8_t aica_rtc[ADDR_AICA_RTC_LAST - ADDR_AICA_RTC_FIRST + 1];
 
 int aica_rtc_read(void *buf, size_t addr, size_t len) {
     addr32_t first_byte = addr;
     addr32_t last_byte = addr + (len - 1);
+
+    // HACK
+    uint32_t tmp;
+    if (addr == 0x710000)
+        tmp = RTC_DEFAULT >> 16;
+    else if (addr == 0x710004)
+        tmp = RTC_DEFAULT & 0xffff;
+    else
+        tmp = 0;
+
     if (first_byte >= ADDR_AICA_RTC_FIRST && first_byte <= ADDR_AICA_RTC_LAST &&
         last_byte >= ADDR_AICA_RTC_FIRST && last_byte <= ADDR_AICA_RTC_LAST) {
-        memcpy(buf, aica_rtc + first_byte, len);
+        memcpy(buf, &tmp, len);
         return 0;
     }
+
+    std::cout << "Reading " << len << " bytes from AICA RTC address 0x" <<
+        std::hex << addr;
 
     BOOST_THROW_EXCEPTION(UnimplementedError() <<
                           errinfo_feature("Whatever happens when "
@@ -53,6 +86,9 @@ int aica_rtc_write(void const *buf, size_t addr, size_t len) {
         memcpy(aica_rtc + first_byte, buf, len);
         return 0;
     }
+
+    std::cout << "Writing " << len << " bytes to AICA RTC address 0x" <<
+        std::hex << addr;
 
     BOOST_THROW_EXCEPTION(UnimplementedError() <<
                           errinfo_feature("Whatever happens when "
