4c23941fa355d597957b58754800f5d3665d1b69
snickerbockers <chimerasaurusrex@gmail.com>
2017-04-22 16:22:38 -0700

    new error handling system for the upcoming C refactor

---
+++ src/error.c
@@ -0,0 +1,104 @@
+/*******************************************************************************
+ *
+ *
+ *    WashingtonDC Dreamcast Emulator
+ *    Copyright (C) 2017 snickerbockers
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ ******************************************************************************/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "error.h"
+
+#ifdef __cplusplus
+#error oh bother
+#endif
+
+static char const *error_type_string(enum error_type tp);
+static void print_attr(struct error_attr const *attr);
+
+enum error_type error_type;
+
+struct error_attr *first_attr;
+
+void error_raise(enum error_type tp) {
+    error_type = tp;
+
+    error_print();
+    exit(1);
+}
+
+void error_clear() {
+    error_type = ERROR_NONE;
+    first_attr = NULL;
+}
+
+enum error_type error_check() {
+    return error_type;
+}
+
+void error_add_attr(struct error_attr *attr) {
+    if (first_attr)
+        first_attr->pprev = &attr->next;
+    attr->next = first_attr;
+    first_attr = attr;
+}
+
+void error_print() {
+    fprintf(stderr, "ERROR: %s\n", error_type_string(error_type));
+
+    struct error_attr *curs = first_attr;
+    while (curs) {
+        print_attr(curs);
+
+        curs = curs->next;
+    }
+}
+
+static void print_attr(struct error_attr const *attr) {
+    switch (attr->tp) {
+    case ERROR_TYPE_STRING:
+        fprintf(stderr, "[%s] = \"%s\"\n", attr->attr_name, attr->val.as_str);
+        break;
+    case ERROR_TYPE_INT:
+        fprintf(stderr, "[%s] = %d\n", attr->attr_name, attr->val.as_int);
+        break;
+    case ERROR_TYPE_U32:
+        fprintf(stderr, "[%s] = %x\n", attr->attr_name, (int)attr->val.as_u32);
+    default:
+        break;
+    }
+}
+
+static char const *error_type_string(enum error_type tp) {
+    switch (tp) {
+    case ERROR_NONE:
+        return "no error";
+    case ERROR_UNIMPLEMENTED:
+        return "unable to continue due to unimplemented functionality";
+    case ERROR_INVALID_PARAM:
+        return "invalid parameter value";
+    default:
+        return "Unknown error (this shouldn\'t happen)";
+    }
+}
+
+DEF_ERROR_INT_ATTR(line)
+DEF_ERROR_STRING_ATTR(file)
+DEF_ERROR_STRING_ATTR(feature)
+DEF_ERROR_STRING_ATTR(param_name)
---
+++ src/error.h
@@ -0,0 +1,137 @@
+/*******************************************************************************
+ *
+ *
+ *    WashingtonDC Dreamcast Emulator
+ *    Copyright (C) 2017 snickerbockers
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ ******************************************************************************/
+
+#ifndef ERROR_H_
+#define ERROR_H_
+
+/* this is not thread safe - only the emulation thread can use it for now */
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+enum error_type {
+    ERROR_NONE = 0,
+
+    /* unable to continue due to unimplemented functionality */
+    ERROR_UNIMPLEMENTED,
+
+    ERROR_INVALID_PARAM
+};
+
+enum error_attr_type {
+    ERROR_TYPE_STRING,
+    ERROR_TYPE_INT,
+    ERROR_TYPE_U32
+};
+
+union error_dat {
+    char const *as_str;
+    int as_int;
+    uint32_t as_u32;
+};
+
+/*
+ * error_attrs should always be global and/or static variables.
+ * They should never be allocated on the stack or the heap.
+ */
+struct error_attr {
+    char const *attr_name;
+
+    enum error_attr_type tp;
+
+    union error_dat val;
+
+    struct error_attr *next;
+    struct error_attr **pprev;
+};
+
+void error_raise(enum error_type tp);
+enum error_type error_check();
+void error_clear();
+
+void error_add_attr(struct error_attr *attr);
+
+void error_print();
+
+#define ERROR_STRING_ATTR(the_attr_name)                \
+    void error_set_##the_attr_name(char const *attr_val)
+
+#define ERROR_INT_ATTR(the_attr_name)                   \
+    void error_set_##the_attr_name(int attr_val)
+
+#define ERROR_U32_ATTR(the_attr_name)                   \
+    void error_set_##the_attr_name(uint32_t attr_val)
+
+#define DEF_ERROR_STRING_ATTR(the_attr_name)            \
+    ERROR_STRING_ATTR(the_attr_name) {                  \
+        static struct error_attr attr;                  \
+                                                        \
+        attr.attr_name = #the_attr_name;                \
+        attr.tp = ERROR_TYPE_STRING;                    \
+        attr.val.as_str = attr_val;                     \
+                                                        \
+        error_add_attr(&attr);                          \
+    }                                                   \
+
+#define DEF_ERROR_INT_ATTR(the_attr_name)               \
+    ERROR_INT_ATTR(the_attr_name) {                     \
+        static struct error_attr attr;                  \
+                                                        \
+        attr.attr_name = #the_attr_name;                \
+        attr.tp = ERROR_TYPE_INT;                       \
+        attr.val.as_int = attr_val;                     \
+                                                        \
+        error_add_attr(&attr);                          \
+    }                                                   \
+
+#define DEF_ERROR_U32_ATTR(the_attr_name)               \
+    ERROR_U32_ATTR(the_attr_name) {                     \
+        static struct error_attr attr;                  \
+                                                        \
+        attr.attr_name = #the_attr_name;                \
+        attr.tp = ERROR_TYPE_INT;                       \
+        attr.val.as_u32 = attr_val;                     \
+                                                        \
+        error_add_attr(&attr);                          \
+    }                                                   \
+
+ERROR_INT_ATTR(line);
+ERROR_STRING_ATTR(file);
+
+ERROR_STRING_ATTR(feature);
+ERROR_STRING_ATTR(param_name);
+
+#define RAISE_ERROR(tp)                         \
+    do {                                        \
+        error_set_line(__LINE__);               \
+        error_set_file(__FILE__);               \
+        error_raise(tp);                        \
+    } while (0)
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
