09f3ae6b2393e8bc4ccdfc46ae3d7825f2410416
snickerbockers <chimerasaurusrex@gmail.com>
2017-06-07 23:13:36 -0700

    unit_tests: implement a test for the single-precision version of FCMP/EQ

--- unit_tests/sh4inst_test.cpp
+++ unit_tests/sh4inst_test.cpp
@@ -11448,6 +11448,66 @@ public:
 
         return failure;
     }
+
+    static int do_binary_fcmpeq_fr_fr(Sh4 *cpu, BiosFile *bios, Memory *mem,
+                                    unsigned src_reg_no, unsigned dst_reg_no,
+                                    float src, float dst) {
+        Sh4Prog test_prog;
+        std::stringstream ss;
+        std::string cmd;
+
+        ss << "FCMP/EQ FR" << src_reg_no << ", FR" << dst_reg_no << "\n";
+        cmd = ss.str();
+        test_prog.add_txt(cmd);
+        const Sh4Prog::ByteList& inst = test_prog.get_prog();
+        bios_load_binary(bios, 0, inst.begin(), inst.end());
+
+        reset_cpu(cpu);
+
+        *sh4_fpu_fr(cpu, src_reg_no) = src;
+        *sh4_fpu_fr(cpu, dst_reg_no) = dst;
+        sh4_exec_inst(cpu);
+
+        bool t_expect = dst == src;
+        bool t_actual = (bool)(cpu->reg[SH4_REG_SR] & SH4_SR_FLAG_T_MASK);
+        if (t_actual != t_expect) {
+            std::cout << "ERROR while running " << cmd << std::endl;
+            std::cout << "expected value of T is " << t_expect << std::endl;
+            std::cout << "actual value is " << t_actual << std::endl;
+            std::cout << "inputs are are " << src << " and " << dst <<
+                std::endl;
+            return 1;
+        }
+        return 0;
+    }
+
+    // FCMP/EQ FRm, FRn
+    // 1111nnnnmmmm0100
+    static int binary_fcmpeq_fr_fr(Sh4 *cpu, BiosFile *bios, Memory *mem,
+                                   RandGen32 *randgen32) {
+        int failure = 0;
+
+        for (unsigned src = 0; src < 16; src++) {
+            for (unsigned dst = 0; dst < 16; dst++) {
+                float src_val, dst_val;
+                src_val = randgen32->pick_double();
+                if (src == dst)
+                    dst_val = src_val;
+                else
+                    dst_val = randgen32->pick_double();
+                failure = failure ||
+                    do_binary_fcmpeq_fr_fr(cpu, bios, mem, src, dst,
+                                           src_val, dst_val);
+
+                // make sure the case where they're equal gets exercised
+                failure = failure ||
+                    do_binary_fcmpeq_fr_fr(cpu, bios, mem, src, dst,
+                                           src_val, src_val);
+            }
+        }
+
+        return failure;
+    }
 };
 
 struct inst_test {
@@ -11678,6 +11738,7 @@ struct inst_test {
     { "binary_fmac_fr0_fr_fr", &Sh4InstTests::binary_fmac_fr0_fr_fr },
     { "unary_fsqrt_fr", &Sh4InstTests::unary_fsqrt_fr },
     { "unary_fsrra_fr", &Sh4InstTests::unary_fsrra_fr },
+    { "binary_fcmpeq_fr_fr", &Sh4InstTests::binary_fcmpeq_fr_fr },
     { NULL }
 };
 
