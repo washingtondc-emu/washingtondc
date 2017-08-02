1874b33b41f60d0e920be165cf56bc391e0b7d92
snickerbockers <chimerasaurusrex@gmail.com>
2017-08-02 01:52:21 -0700

    flash memory: log all read/write accesses
    Seems to be some sort of protocol to this.  all writes start with aa then 55.
    Must investigate further; this could be causing github issue #13 if the
    firmware is writing the language setting to the flash and subsequently reading
    it back.

--- src/flash_memory.c
+++ src/flash_memory.c
@@ -23,12 +23,18 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
+#include <stdarg.h>
 #include <errno.h>
 
 #include "mem_code.h"
 #include "error.h"
 
 #include "flash_memory.h"
+
+#define FLASH_MEM_TRACE(msg, ...) flash_mem_do_trace(msg, ##__VA_ARGS__)
+
+// don't call this directly, use the FLASH_MEM_TRACE macro instead
+void flash_mem_do_trace(char const *msg, ...);
 
 static uint8_t flash_mem[FLASH_MEM_SZ];
 
@@ -63,9 +69,10 @@ void flash_mem_load(char const *path) {
     }
 
     if (file_len != FLASH_MEM_SZ) {
-        printf("WARNING - unexpected flash memory size (expected %d bytes, "
-               "got %d bytes)).  This will still be loaded even though it\'s "
-               "incorrect\n", FLASH_MEM_SZ, (int)file_len);
+        FLASH_MEM_TRACE("WARNING - unexpected flash memory size (expected %d "
+                        "bytes, got %d bytes)).  This will still be loaded "
+                        "even though it\'s incorrect\n",
+                        FLASH_MEM_SZ, (int)file_len);
     }
 
     if (file_len > FLASH_MEM_SZ || file_len < 0)
@@ -89,6 +96,23 @@ int flash_mem_read(void *buf, size_t addr, size_t len) {
         return MEM_ACCESS_FAILURE;
     }
 
+    if (len == 1) {
+        uint8_t val;
+        memcpy(&val, flash_mem + (addr - ADDR_FLASH_FIRST), sizeof(val));
+        FLASH_MEM_TRACE("read %02x from %08x\n", (unsigned)val, (unsigned)addr);
+    } else if (len == 2) {
+        uint16_t val;
+        memcpy(&val, flash_mem + (addr - ADDR_FLASH_FIRST), sizeof(val));
+        FLASH_MEM_TRACE("read %04x from %08x\n", (unsigned)val, (unsigned)addr);
+    } else if (len == 4) {
+        uint32_t val;
+        memcpy(&val, flash_mem + (addr - ADDR_FLASH_FIRST), sizeof(val));
+        FLASH_MEM_TRACE("read %08x from %08x\n", (unsigned)val, (unsigned)addr);
+    } else {
+        FLASH_MEM_TRACE("read %08x bytes from %08x\n",
+                        (unsigned)len, (unsigned)addr);
+    }
+
     memcpy(buf, flash_mem + (addr - ADDR_FLASH_FIRST), len);
 
     return MEM_ACCESS_SUCCESS;
@@ -102,7 +126,35 @@ int flash_mem_write(void const *buf, size_t addr, size_t len) {
         return MEM_ACCESS_FAILURE;
     }
 
+    if (len == 1) {
+        uint8_t val;
+        memcpy(&val, buf, sizeof(val));
+        FLASH_MEM_TRACE("write %02x to %08x\n", (unsigned)val, (unsigned)addr);
+    } else if (len == 2) {
+        uint16_t val;
+        memcpy(&val, buf, sizeof(val));
+        FLASH_MEM_TRACE("write %04x to %08x\n", (unsigned)val, (unsigned)addr);
+    } else if (len == 4) {
+        uint32_t val;
+        memcpy(&val, buf, sizeof(val));
+        FLASH_MEM_TRACE("write %08x to %08x\n", (unsigned)val, (unsigned)addr);
+    } else {
+        FLASH_MEM_TRACE("write %08x bytes to %08x\n",
+                        (unsigned)len, (unsigned)addr);
+    }
+
     memcpy(flash_mem + (addr - ADDR_FLASH_FIRST), buf, len);
 
     return MEM_ACCESS_SUCCESS;
+}
+
+void flash_mem_do_trace(char const *msg, ...) {
+    va_list var_args;
+    va_start(var_args, msg);
+
+    printf("FLASH_MEM: ");
+
+    vprintf(msg, var_args);
+
+    va_end(var_args);
 }
