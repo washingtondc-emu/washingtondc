bb9dbddc7162e877ed73b94834e8100cec088cd3
snickerbockers <chimerasaurusrex@gmail.com>
2017-08-03 22:35:14 -0700

    remove the locking from the geo_buf ringbuffer
    The producer will now spin when the ringbuffer is full.

--- src/hw/pvr2/geo_buf.c
+++ src/hw/pvr2/geo_buf.c
@@ -22,7 +22,6 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
 
 #include "dreamcast.h"
 
@@ -30,16 +29,12 @@
 
 #define GEO_BUF_COUNT 4
 
-static pthread_cond_t cons_adv = PTHREAD_COND_INITIALIZER;
-static pthread_mutex_t cons_mtx = PTHREAD_MUTEX_INITIALIZER;
-
 // producer and consumer indices
 static volatile unsigned prod_idx, cons_idx;
 
 static struct geo_buf ringbuf[GEO_BUF_COUNT];
 
 static unsigned next_frame_stamp;
-static unsigned last_prod_frame_stamp;
 
 DEF_ERROR_INT_ATTR(src_blend_factor);
 DEF_ERROR_INT_ATTR(dst_blend_factor);
@@ -47,21 +42,10 @@ DEF_ERROR_INT_ATTR(display_list_index);
 DEF_ERROR_INT_ATTR(geo_buf_group_index);
 
 struct geo_buf *geo_buf_get_cons(void) {
-    struct geo_buf *ret;
-
-    // TODO: maybe I don't need to grab the mutex here since it's read-only
-    if (pthread_mutex_lock(&cons_mtx) != 0)
-        abort(); // TODO: error handling
-
     if (prod_idx == cons_idx)
-        ret = NULL;
+        return NULL;
     else
-        ret = ringbuf + cons_idx;
-
-    if (pthread_mutex_unlock(&cons_mtx) != 0)
-        abort(); // TODO: error handling
-
-    return ret;
+        return ringbuf + cons_idx;
 }
 
 struct geo_buf *geo_buf_get_prod(void) {
@@ -69,20 +53,12 @@ struct geo_buf *geo_buf_get_prod(void) {
 }
 
 void geo_buf_consume(void) {
-    if (pthread_mutex_lock(&cons_mtx) != 0)
-        abort(); // TODO: error handling
-
     if (prod_idx == cons_idx) {
         fprintf(stderr, "WARNING: attempt to consume from empty geo_buf "
                 "ring\n");
     } else {
         cons_idx = (cons_idx + 1) % GEO_BUF_COUNT;
-        if (pthread_cond_signal(&cons_adv) != 0)
-            abort(); // TODO: error handling
     }
-
-    if (pthread_mutex_unlock(&cons_mtx) != 0)
-        abort(); // TODO: error handling
 }
 
 static void init_geo_buf(struct geo_buf *buf) {
@@ -104,25 +80,15 @@ static void init_geo_buf(struct geo_buf *buf) {
 void geo_buf_produce(void) {
     unsigned next_prod_idx = (prod_idx + 1) % GEO_BUF_COUNT;
 
-    if (pthread_mutex_lock(&cons_mtx) != 0)
-        abort(); // TODO: error handling
     if (next_prod_idx == cons_idx) {
-        /*
-         * TODO: this could cause deadlocks maybe since last_prod_frame_stamp
-         * will be incorrect
-         */
         fprintf(stderr, "WARNING %s: prod_idx == %u, cons_idx == %u.  "
-                "Waiting for ring to drain...\n", __func__, prod_idx, cons_idx);
+                "This thread will spin while the ring drains...\n", __func__, prod_idx, cons_idx);
         while (next_prod_idx == cons_idx && dc_is_running())
-            pthread_cond_wait(&cons_adv, &cons_mtx);
+            ;
         fprintf(stderr, "the ring has drained\n");
     }
 
     prod_idx = next_prod_idx;
-    last_prod_frame_stamp = ringbuf[prod_idx].frame_stamp;
-
-    if (pthread_mutex_unlock(&cons_mtx) != 0)
-        abort(); // TODO: error handling
 
     init_geo_buf(ringbuf + prod_idx);
 }
