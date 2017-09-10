55b1c8cfb0f51c4a28e188381c1942a02f434f0d
snickerbockers <chimerasaurusrex@gmail.com>
2017-09-09 20:57:46 -0700

    convert pvr2_ta_vert.glsl into a single matrix-multiplication.

--- src/gfx/opengl/pvr2_ta_vert.glsl
+++ src/gfx/opengl/pvr2_ta_vert.glsl
@@ -51,50 +51,24 @@ void tex_transform() {
  * screen-coordinates with an origin in the upper-left) to OpenGL
  * coordinates (which are bounded from -1.0 to 1.0, with the upper-left
  * coordinate being at (-1.0, 1.0)
- *
- * TODO: there's no reason why this entire function couldn't be replaced with
- * a single matrix transformation.
  */
 void modelview_project_transform() {
-    float vert_x = (vert_pos.x - half_screen_dims.x) / half_screen_dims.x;
-    float vert_y = -(vert_pos.y - half_screen_dims.y) / half_screen_dims.y;
-
-    float clip_half = (clip_min_max[1] - clip_min_max[0]) * 0.5f;
-
     /*
-     * TODO: I'm not 100% certain I understand why I'm flipping the
-     * z-coordinates here.  I think it has to be done because in OpenGL
-     * clip-coordinates,  the positive side of the z-axis is closer to the
-     * screen and the negative side is farther away.
-     *
-     * Ultimately, this is something I am doing because I have to do it to get
-     * the glDepthFunc functions to behave the way I expect them to; I'm just
-     * not 100% sure if the math is actually working out the way I think it is.
-     * It does seem weird to multiply the z-coordinates by -1 like this.
-     *
-     * HYPOTHESIS:
-     * the math here is a little wonky because what we see as the Z-value
-     * is actually 1 / z.  The perspective divide would have already been done
-     * by the guest software before it passed the coordinates to the TA, so this
-     * only really effects the depth-sorting.  Nevertheless, this may need to be
-     * revisitied in the future since this code was all written under the
-     * assumption that the z-coordinate is actually the z-coordinate.
-     *
-     * the way we quantize the vertex from (clip_min_max[0], clip_min_max[1])
-     * to (-1.0, 1.0) is especially an area of concern because mip_min_max
-     * represents the minimum/maximum values of 1/z instead of z, but I thought
-     * it was z when I write this code.
-     *
-     * This could very well be why I have to multiply vert_z by -1.  if
-     * vert_pos.z is clip min_max[0], then vert_z will be -1.0, which would mean
-     * that in clip-coordinates it's the closest vertex to the camera even
-     * though having a minimal 1/z means that Z is very large (far away from
-     * the camera).
+     * Orthographic projection.  Map all coordinates into the (-1, -1, -1) to
+     * (1, 1, 1).  Anything less than -half_screen_dims or greater than
+     * half_screen_dims on the x/y axes or anything not between
+     * clip_min_max[0]/clip_min_max[1] on the z-axis will be clipped.  Ideally
+     * nothing should be clipped on the z-axis because clip_min_max is derived
+     * from the minimum and maximum depths.
      */
-    float vert_z = -(vert_pos.z - clip_half) /
-        (clip_min_max[1] - clip_min_max[0]);
+    mat4 ortho2d = mat4(
+                        vec4(1.0 / half_screen_dims.x, 0, 0, 0),
+                        vec4(0, -1.0 / half_screen_dims.y, 0, 0),
+                        vec4(0, 0, -1.0 / (clip_min_max[1] - clip_min_max[0]), 0),
+                        vec4(-1, 1, clip_min_max[0] / (clip_min_max[1] - clip_min_max[0]), 1)
+                        );
 
-    gl_Position = vec4(vert_x, vert_y, vert_z, 1.0);
+    gl_Position = ortho2d * vec4(vert_pos, 1);
 }
 
 void color_transform() {
