75b49d357c39873b353fadc9e9be55062ced5e1d
snickerbockers <chimerasaurusrex@gmail.com>
2017-12-27 16:51:24 -0800

    Correct the copyright on src/BiosFile.c to include 2017
    During 2017, changes were made to this file in commits
    fbbfc6657e3be6df65c30f2dbe5e9cdf7519272b,
    ebc7ead7ff971e1cb0bbc9e2a48a5535752ace87, and
    86ae6a92a898c37ebdcb179d9263b500b8309e1e.
    
    There might have been other commits to this file in 2017, as well; the three I
    listed only go back to when I ported it from C++ to C.

--- src/BiosFile.c
+++ src/BiosFile.c
@@ -2,7 +2,7 @@
  *
  *
  *    WashingtonDC Dreamcast Emulator
- *    Copyright (C) 2016 snickerbockers
+ *    Copyright (C) 2016, 2017 snickerbockers
  *
  *    This program is free software: you can redistribute it and/or modify
  *    it under the terms of the GNU General Public License as published by
