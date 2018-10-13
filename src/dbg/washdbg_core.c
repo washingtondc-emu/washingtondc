635b9ab6e3d529cdb41bbb80be167097008ee924
snickerbockers <chimerasaurusrex@gmail.com>
2018-10-13 12:48:12 -0700

    WashDbg: implement the x command

--- src/dbg/washdbg_core.c
+++ src/dbg/washdbg_core.c
@@ -23,6 +23,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 #include "log.h"
 #include "dreamcast.h"
@@ -49,6 +50,19 @@ static void washdbg_print_banner(void);
 
 static void washdbg_echo(int argc, char **argv);
 
+static void washdbg_state_echo_process(void);
+
+static int
+eval_expression(char const *expr, enum dbg_context_id *ctx_id, unsigned *out);
+
+static unsigned washdbg_print_x(void);
+
+static int
+parse_fmt_string(char const *str, unsigned *byte_count_out,
+                 unsigned *count_out);
+
+static void unsigned_to_hex(char *out, size_t n_chars, unsigned val);
+
 enum washdbg_state {
     WASHDBG_STATE_BANNER,
     WASHDBG_STATE_PROMPT,
@@ -60,6 +74,7 @@ enum washdbg_state {
     WASHDBG_STATE_CONTEXT_INFO,
     WASHDBG_STATE_PRINT_ERROR,
     WASHDBG_STATE_ECHO,
+    WASHDBG_STATE_X,
 
     // permanently stop accepting commands because we're about to disconnect.
     WASHDBG_STATE_CMD_EXIT
@@ -80,10 +95,19 @@ void washdbg_do_continue(int argc, char **argv) {
     cur_state = WASHDBG_STATE_CMD_CONTINUE;
 }
 
+static bool washdbg_is_continue_cmd(char const *cmd) {
+    return strcmp(cmd, "c") == 0 ||
+        strcmp(cmd, "continue") == 0;
+}
+
 void washdbg_do_exit(int argc, char **argv) {
     LOG_INFO("User requested exit via WashDbg\n");
     dreamcast_kill();
     cur_state = WASHDBG_STATE_CMD_EXIT;
+}
+
+static bool washdbg_is_exit_cmd(char const *cmd) {
+    return strcmp(cmd, "exit") == 0;
 }
 
 void washdbg_input_ch(char ch) {
@@ -125,11 +149,16 @@ void washdbg_do_help(int argc, char **argv) {
         "continue - continue execution when suspended.\n"
         "echo     - echo back text\n"
         "exit     - exit the debugger and close WashingtonDC\n"
-        "help     - display this message\n";
+        "help     - display this message\n"
+        "x        - eXamine memory address\n";
 
     help_state.txt.txt = help_msg;
     help_state.txt.pos = 0;
     cur_state = WASHDBG_STATE_HELP;
+}
+
+static bool washdbg_is_help_cmd(char const *cmd) {
+    return strcmp(cmd, "help") == 0;
 }
 
 struct context_info_state {
@@ -238,7 +267,121 @@ cleanup_args:
     free(echo_state.argv);
 }
 
-    static void washdbg_state_echo_process(void);
+static bool washdbg_is_echo_cmd(char const *cmd) {
+    return strcmp(cmd, "echo") == 0;
+}
+
+static struct x_state {
+    char str[32];
+    size_t str_pos;
+
+    void *dat;
+    unsigned byte_count;
+    unsigned count;
+    unsigned idx;
+} x_state;
+
+static void washdbg_x_set_string(void) {
+    uint32_t val32;
+    uint16_t val16;
+    uint8_t val8;
+    switch (x_state.byte_count) {
+    case 4:
+        val32 = ((uint32_t*)x_state.dat)[x_state.idx];
+        snprintf(x_state.str, sizeof(x_state.str), "0x%08x\n", (unsigned)val32);
+        break;
+    case 2:
+        val16 = ((uint16_t*)x_state.dat)[x_state.idx];
+        snprintf(x_state.str, sizeof(x_state.str), "0x%04x\n", (unsigned)val16);
+        break;
+    case 1:
+        val8 = ((uint8_t*)x_state.dat)[x_state.idx];
+        snprintf(x_state.str, sizeof(x_state.str), "0x%02x\n", (unsigned)val8);
+        break;
+    default:
+        strncpy(x_state.str, "<ERROR>\n", sizeof(x_state.str));
+        x_state.str[31] = '\0';
+    }
+}
+
+static void washdbg_x(int argc, char **argv) {
+    unsigned addr;
+    enum dbg_context_id ctx;
+    char *fmt_str;
+
+    if (argc != 2) {
+        washdbg_print_error("only a single argument is supported for the x "
+                            "command.\n");
+        return;
+    }
+
+    fmt_str = strchr(argv[0], '/') + 1;
+    if (fmt_str)
+        printf("The format string is \"%s\"\n", fmt_str);
+
+    memset(&x_state, 0, sizeof(x_state));
+
+    if (parse_fmt_string(fmt_str, &x_state.byte_count, &x_state.count) < 0) {
+        washdbg_print_error("failed to parse x-command format string.\n");
+        return;
+    }
+
+    if (eval_expression(argv[1], &ctx, &addr) != 0)
+        return;
+
+    if (x_state.count > 1024 * 32) {
+        washdbg_print_error("too much data\n");
+        return;
+    }
+
+    x_state.dat = calloc(x_state.byte_count, x_state.count);
+    if (!x_state.dat) {
+        washdbg_print_error("failed allocation\n");
+        return;
+    }
+
+    // now do the memory lookup here
+    if (debug_read_mem(ctx, x_state.dat, addr,
+                       x_state.byte_count * x_state.count) < 0) {
+        washdbg_print_error("only a single argument is supported for the x "
+                            "command.\n");
+        return;
+    }
+
+    washdbg_x_set_string();
+    x_state.idx = 1;
+
+    cur_state = WASHDBG_STATE_X;
+}
+
+static unsigned washdbg_print_x(void) {
+    char const *start = x_state.str + x_state.str_pos;
+    size_t len = strlen(x_state.str);
+    unsigned rem_chars = len - x_state.str_pos;
+    if (rem_chars) {
+        unsigned n_chars = washdbg_puts(start);
+        if (n_chars == rem_chars)
+            goto reload;
+        else
+            x_state.str_pos += n_chars;
+    } else {
+        goto reload;
+    }
+    return 1;
+
+reload:
+    if (x_state.idx == x_state.count)
+        return 0;
+    washdbg_x_set_string();
+    x_state.idx++;
+    return 1;
+}
+
+static bool washdbg_is_x_cmd(char const *cmd) {
+    // TODO: implement formatted versions, like x/w
+    return strcmp(cmd, "x") == 0 ||
+        (strlen(cmd) && cmd[0] == 'x' && cmd[1] == '/');
+}
 
 void washdbg_core_run_once(void) {
     switch (cur_state) {
@@ -277,6 +420,13 @@ void washdbg_core_run_once(void) {
         break;
     case WASHDBG_STATE_ECHO:
         washdbg_state_echo_process();
+        break;
+    case WASHDBG_STATE_X:
+        if (washdbg_print_x() == 0) {
+            free(x_state.dat);
+            x_state.dat = NULL;
+            washdbg_print_prompt();
+        }
         break;
     default:
         break;
@@ -351,17 +501,20 @@ static void washdbg_process_input(void) {
         else
             cmd = "";
 
-        if (strcmp(cmd, "continue") == 0 ||
-            strcmp(cmd, "c") == 0) {
-            washdbg_do_continue(argc, argv);
-        } else if (strcmp(cmd, "exit") == 0) {
-            washdbg_do_exit(argc, argv);
-        } else if (strcmp(cmd, "help") == 0) {
-            washdbg_do_help(argc, argv);
-        } else if (strcmp(cmd, "echo") == 0) {
-            washdbg_echo(argc, argv);
-        } else if (strlen(cmd)) {
-            washdbg_bad_input(cmd);
+        if (strlen(cmd)) {
+            if (washdbg_is_continue_cmd(cmd)) {
+                washdbg_do_continue(argc, argv);
+            } else if (washdbg_is_exit_cmd(cmd)) {
+                washdbg_do_exit(argc, argv);
+            } else if (washdbg_is_help_cmd(cmd)) {
+                washdbg_do_help(argc, argv);
+            } else if (washdbg_is_echo_cmd(cmd)) {
+                washdbg_echo(argc, argv);
+            } else if (washdbg_is_x_cmd(cmd)) {
+                washdbg_x(argc, argv);
+            } else {
+                washdbg_bad_input(cmd);
+            }
         } else {
             washdbg_print_prompt();
         }
@@ -436,4 +589,242 @@ static unsigned washdbg_print_buffer(struct washdbg_txt_state *state) {
         return 0;
     }
     return strlen(state->txt) - state->pos;
+}
+
+static bool is_hex_str(char const *str) {
+    if (*str == 0)
+        return false; // empty string
+    while (*str)
+        switch (*str++) {
+        case '0':
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+        case 'a':
+        case 'A':
+        case 'b':
+        case 'B':
+        case 'c':
+        case 'C':
+        case 'd':
+        case 'D':
+        case 'e':
+        case 'E':
+        case 'f':
+        case 'F':
+            break;
+        default:
+            return false;
+        }
+    return true;
+}
+
+static bool is_dec_str(char const *str) {
+    if (*str == 0)
+        return false; // empty string
+    while (*str)
+        if (!isdigit(*str++))
+            return false;
+    return true;
+}
+
+static unsigned parse_dec_str(char const *str) {
+    size_t len = strlen(str);
+    if (!len)
+        return 0; // error condition; just ignore it for now
+    size_t idx = len - 1;
+    unsigned total = 0;
+    unsigned scale = 1;
+    do {
+        unsigned weight = str[idx] - '0';
+        total += scale * weight;
+        scale *= 10;
+    } while (idx--);
+    return total;
+}
+
+static unsigned parse_hex_str(char const *str) {
+    size_t len = strlen(str);
+    if (!len)
+        return 0; // error condition; just ignore it for now
+    size_t idx = len - 1;
+    unsigned total = 0;
+    unsigned scale = 1;
+    do {
+        /* unsigned weight = str[idx] - '0'; */
+        unsigned weight;
+        if (str[idx] >= '0' && str[idx] <= '9')
+            weight = str[idx] - '0';
+        else if (str[idx] >= 'a' && str[idx] <= 'f')
+            weight = str[idx] - 'a' + 10;
+        else if (str[idx] >= 'A' && str[idx] <= 'F')
+            weight = str[idx] - 'A' + 10;
+        else
+            weight = 0; // error condition; just ignore it for now
+        total += scale * weight;
+        scale *= 16;
+    } while (idx--);
+    printf("%s is %u\n", str, total);
+    return total;
+}
+
+/*
+ * expression format:
+ * <ctx>:0xhex_val
+ * OR
+ * <ctx>:dec_val
+ * OR
+ * <ctx>:$reg_name
+ *
+ * ctx can be arm7 or sh4.  If it is not provided, it defaults to the current
+ * context.  If the command interprets the value as being a pointer, then ctx
+ * indicates whether it points to arm7's memory space or sh4's memory space.
+ *
+ * If the command does not interpret the value as a pointer, then ctx only
+ * matters for the $reg_name form.  However, ctx can still be speciified even
+ * though it is useless.
+ */
+static int eval_expression(char const *expr, enum dbg_context_id *ctx_id, unsigned *out) {
+    enum dbg_context_id ctx = debug_current_context();
+
+    char *first_colon = strchr(expr, ':');
+    if (first_colon != NULL) {
+        unsigned n_chars = first_colon - expr;
+        if (n_chars == 3 && toupper(expr[0]) == 'S' &&
+            toupper(expr[1]) == 'H' && toupper(expr[2]) == '4') {
+            ctx = DEBUG_CONTEXT_SH4;
+        } else if (n_chars == 4 && toupper(expr[0]) == 'A' &&
+                   toupper(expr[1]) == 'R' && toupper(expr[2]) == 'M' &&
+                   toupper(expr[3]) == '7') {
+            ctx = DEBUG_CONTEXT_ARM7;
+        } else {
+            // unrecognized context
+            washdbg_print_error("Unknown context\n");
+            return -1;
+        }
+
+        expr = first_colon + 1;
+    }
+
+    *ctx_id = ctx;
+
+    if (strlen(expr) == 0) {
+        washdbg_print_error("empty expression\n");
+        return -1;
+    }
+
+    if (expr[0] == '$') {
+        // register
+        washdbg_print_error("register expressions are not implemented yet\n");
+        return -1;
+    } else if (expr[0] == '0' && toupper(expr[1]) == 'X' &&
+               is_hex_str(expr + 2)) {
+        // hex
+        *out = parse_hex_str(expr);
+        return 0;
+    } else if (is_dec_str(expr)) {
+        // decimal
+        *out = parse_dec_str(expr);
+        return 0;
+    } else {
+        // error
+        washdbg_print_error("unknown expression class\n");
+        return -1;
+    }
+}
+
+__attribute__((unused))
+static void unsigned_to_hex(char *out, size_t n_chars, unsigned val) {
+    snprintf(out, n_chars, "0x%08x\n", val);
+    out[n_chars - 1] = '\0';
+}
+
+static int
+parse_fmt_string(char const *str, unsigned *byte_count_out,
+                 unsigned *count_out) {
+    bool have_count = false;
+    bool have_byte_count = false;
+    unsigned byte_count = 4;
+    unsigned count = 1;
+
+    bool parsing_digits = false;
+
+    char const *digit_start = NULL;
+
+    while (*str || parsing_digits) {
+        if (parsing_digits) {
+            if (*str < '0' || *str > '9') {
+                parsing_digits = false;
+                unsigned n_chars = str - digit_start + 1;
+                if (n_chars >= 32)
+                    return -1;
+                char tmp[32];
+                strncpy(tmp, digit_start, sizeof(tmp));
+                tmp[31] = '\0';
+                if (have_count)
+                    return -1;
+                have_count = true;
+                count = atoi(tmp);
+
+                continue;
+            }
+        } else {
+            switch (*str) {
+            case 'w':
+                if (have_byte_count)
+                    return -1;
+                byte_count = 4;
+                have_byte_count = true;
+                break;
+            case 'h':
+                if (have_byte_count)
+                    return -1;
+                byte_count = 2;
+                have_byte_count = true;
+                break;
+            case 'b':
+                if (have_byte_count)
+                    return -1;
+                byte_count = 1;
+                have_byte_count = true;
+                break;
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                parsing_digits = true;
+                digit_start = str;
+                continue;
+            default:
+                return -1;
+            }
+        }
+        str++;
+    }
+
+    /*
+     * This limit is arbitrary, you can increase or decrease it as you'd like.
+     * I just put this in there to keep things sane.
+     */
+    if (count >= 2048) {
+        washdbg_print_error("too much data\n");
+        return -1;
+    }
+
+    *count_out = count;
+    *byte_count_out = byte_count;
+
+    return 0;
 }
