3ed7f9f458da269022a0546d19581ab45e41d9fa
snickerbockers <chimerasaurusrex@gmail.com>
2019-01-02 12:12:27 -0800

    update src/hw/g2/g2_reg.c copyright
    this should have been done in 76a3556b9b636b6d34400e23876e3838f3d7ae91 .

--- src/hw/g2/g2_reg.c
+++ src/hw/g2/g2_reg.c
@@ -2,7 +2,7 @@
  *
  *
  *    WashingtonDC Dreamcast Emulator
- *    Copyright (C) 2017, 2018 snickerbockers
+ *    Copyright (C) 2017-2019 snickerbockers
  *
  *    This program is free software: you can redistribute it and/or modify
  *    it under the terms of the GNU General Public License as published by
