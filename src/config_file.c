washingtondc-emu/washingtondc

commit cb28dbf79023a25ab25df3cb30f2d394e5f174c0

Optional fullscreen mode.
On my PC, this doesn't actually work and goes into borderless fullscreen
instead.  This also happens on my PC with the default glfw sample apps, so this
is potentiall a glfw bug.  Hopefully it does not also happen on other peoples'
PCs.

src/config_file.c
@@ -187,6 +187,12 @@ static void cfg_create_default_config(void) {
         "win.external-res.x 640\n"
         "win.external-res.y 480\n"
         "\n"
+        "Pick what kind of window you want.\n"
+        "; available values are:\n"
+        "; * windowed\n"
+        "; * fullscreen\n"
+        "win.window-mode windowed\n"
+        "\n"
         "; don't change this line.  It doesn't techincally do anything yet\n"
         "; but it will in future revisions of WashingtonDC.\n"
         "wash.dc.port.0.0 dreamcast_controller\n"

src/glfw/window.c
@@ -73,6 +73,11 @@ static int bind_ctrl_from_cfg(char const *name, char const *cfg_node) {
     return -1;
 }
 
+enum win_mode {
+    WIN_MODE_WINDOWED,
+    WIN_MODE_FULLSCREEN
+};
+
 void win_init(unsigned width, unsigned height) {
     res_x = width;
     res_y = height;
@@ -86,7 +91,32 @@ void win_init(unsigned width, unsigned height) {
     glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
     glfwWindowHint(GLFW_DEPTH_BITS, 24);
 
-    win = glfwCreateWindow(res_x, res_y, title_get(), NULL, NULL);
+    char const *win_mode_str = cfg_get_node("win.window-mode");
+    enum win_mode win_mode;
+
+    if (win_mode_str) {
+        if (strcmp(win_mode_str, "fullscreen") == 0) {
+            win_mode = WIN_MODE_FULLSCREEN;
+        } else if (strcmp(win_mode_str, "windowed") == 0) {
+            win_mode = WIN_MODE_WINDOWED;
+        } else {
+            LOG_ERROR("Unrecognized window mode \"%s\" - using \"windowed\" "
+                      "mode instead\n", win_mode_str);
+            win_mode = WIN_MODE_WINDOWED;
+        }
+    } else {
+        win_mode = WIN_MODE_WINDOWED;
+    }
+
+    switch (win_mode) {
+    case WIN_MODE_FULLSCREEN:
+        win = glfwCreateWindow(res_x, res_y, title_get(), glfwGetPrimaryMonitor(), NULL);
+        break;
+    default:
+    case WIN_MODE_WINDOWED:
+        win = glfwCreateWindow(res_x, res_y, title_get(), NULL, NULL);
+        break;
+    }
 
     if (!win)
         errx(1, "unable to create window");

commit 9e31a67229cb2e4c9497510fa287bccc756c0d32

cfg: fix an invalid comment line in the default config.
This bug was introduced in commit cb28dbf79023a25ab25df3cb30f2d394e5f174c0.

src/config_file.c
@@ -187,7 +187,7 @@ static void cfg_create_default_config(void) {
         "win.external-res.x 640\n"
         "win.external-res.y 480\n"
         "\n"
-        "Pick what kind of window you want.\n"
+        "; Pick what kind of window you want.\n"
         "; available values are:\n"
         "; * windowed\n"
         "; * fullscreen\n"