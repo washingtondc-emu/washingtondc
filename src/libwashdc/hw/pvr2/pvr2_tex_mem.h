cd0b7af78ac8eb3eaf43b4c1524b347c323d611c
snickerbockers <snickerbockers@washemu.org>
2020-01-18 18:31:13 -0800

    unification of 64-bit and 32-bit vram access areas
    With this commit, the pvr2_tex_mem test (as it currently exists, which doesn't
    include DMA and SQ tests) passes.

--- src/libwashdc/hw/pvr2/pvr2_tex_mem.h
+++ src/libwashdc/hw/pvr2/pvr2_tex_mem.h
@@ -31,18 +31,44 @@
 #include "mem_areas.h"
 #include "washdc/MemoryMap.h"
 
-/*
- * I don't yet understand the 32-bit/64-bit access area dichotomy, so I'm
- * keeping them separated for now.  They might both map th the same memory, I'm
- * just not sure yet.
- */
 struct pvr2_tex_mem {
     uint8_t tex32[ADDR_TEX32_LAST - ADDR_TEX32_FIRST + 1];
-    uint8_t tex64[ADDR_TEX64_LAST - ADDR_TEX64_FIRST + 1];
 };
 
 #define PVR2_TEX32_MEM_LEN (ADDR_TEX32_LAST - ADDR_TEX32_FIRST + 1)
 #define PVR2_TEX64_MEM_LEN (ADDR_TEX64_LAST - ADDR_TEX64_FIRST + 1)
+
+#define PVR2_TEX_MEM_BANK_SIZE (PVR2_TEX32_MEM_LEN / 2)
+
+/* static inline unsigned */
+/* pvr2_tex_mem_addr_32_to_64(unsigned offs) { */
+/*     unsigned offs32 = offs & 0x007ffffc; */
+/*     unsigned offs64; */
+
+/*     if (offs32 >= PVR2_TEX_MEM_BANK_SIZE) */
+/*         offs64 = (offs32 - PVR2_TEX_MEM_BANK_SIZE) * 2 + 4; */
+/*     else */
+/*         offs64 = offs32 * 2; */
+
+/*     return offs64 | (offs & 3); */
+/* } */
+
+static inline unsigned
+pvr2_tex_mem_addr_64_to_32(unsigned offs) {
+    unsigned offs64 = offs & 0x007ffffc;
+    unsigned offs32;
+
+    if (offs64 % 8)
+        offs32 = ((offs64 - 4) / 2) + PVR2_TEX_MEM_BANK_SIZE;
+    else
+        offs32 = offs64 / 2;
+
+    return offs32 | (offs & 3);
+}
+
+inline static void
+pvr2_tex_mem_64bit_write8(struct pvr2_tex_mem *mem,
+                          unsigned addr, uint8_t val);
 
 // generic read/write functions for emulator code to use (ie not part of memory map
 inline static double
@@ -220,7 +246,11 @@ pvr2_tex_mem_64bit_read_double(struct pvr2_tex_mem *mem, unsigned addr) {
         RAISE_ERROR(ERROR_INTEGRITY);
     }
 
-    memcpy(&ret, mem->tex64 + addr, sizeof(ret));
+    unsigned offs1 = pvr2_tex_mem_addr_64_to_32(addr);
+    unsigned offs2 = pvr2_tex_mem_addr_64_to_32(addr + 4);
+
+    memcpy(&ret, mem->tex32 + offs1, sizeof(ret)/2);
+    memcpy(((char*)&ret) + sizeof(ret)/2, mem->tex32 + offs2, sizeof(ret)/2);
     return ret;
 }
 
@@ -235,7 +265,8 @@ pvr2_tex_mem_64bit_read_float(struct pvr2_tex_mem *mem, unsigned addr) {
         RAISE_ERROR(ERROR_INTEGRITY);
     }
 
-    memcpy(&ret, mem->tex64 + addr, sizeof(ret));
+    unsigned offs = pvr2_tex_mem_addr_64_to_32(addr);
+    memcpy(&ret, mem->tex32 + offs, sizeof(ret));
     return ret;
 }
 
@@ -250,7 +281,8 @@ pvr2_tex_mem_64bit_read32(struct pvr2_tex_mem *mem, unsigned addr) {
         RAISE_ERROR(ERROR_INTEGRITY);
     }
 
-    memcpy(&ret, mem->tex64 + addr, sizeof(ret));
+    unsigned offs = pvr2_tex_mem_addr_64_to_32(addr);
+    memcpy(&ret, mem->tex32 + offs, sizeof(ret));
     return ret;
 }
 
@@ -265,7 +297,8 @@ pvr2_tex_mem_64bit_read16(struct pvr2_tex_mem *mem, unsigned addr) {
         RAISE_ERROR(ERROR_INTEGRITY);
     }
 
-    memcpy(&ret, mem->tex64 + addr, sizeof(ret));
+    unsigned offs = pvr2_tex_mem_addr_64_to_32(addr);
+    memcpy(&ret, mem->tex32 + offs, sizeof(ret));
     return ret;
 }
 
@@ -280,32 +313,50 @@ pvr2_tex_mem_64bit_read8(struct pvr2_tex_mem *mem, unsigned addr) {
         RAISE_ERROR(ERROR_INTEGRITY);
     }
 
-    memcpy(&ret, mem->tex64 + addr, sizeof(ret));
+    unsigned offs = pvr2_tex_mem_addr_64_to_32(addr);
+    memcpy(&ret, mem->tex32 + offs, sizeof(ret));
     return ret;
 }
 
 inline static void pvr2_tex_mem_64bit_read_raw(struct pvr2_tex_mem *mem,
                                                void *dstp, uint32_t addr,
                                                unsigned n_bytes) {
-    void *srcp = mem->tex64 + addr;
     if (addr + (n_bytes - 1) >= PVR2_TEX64_MEM_LEN) {
         error_set_address(addr);
         error_set_length(n_bytes);
         RAISE_ERROR(ERROR_INTEGRITY);
     }
-    memcpy(dstp, srcp, n_bytes * sizeof(uint8_t));
+
+    uint8_t *dst = (uint8_t*)dstp;
+    while (n_bytes) {
+        uint8_t val = pvr2_tex_mem_64bit_read8(mem, addr);
+        memcpy(dst, &val, sizeof(uint8_t));
+
+        --n_bytes;
+        ++addr;
+        ++dst;
+    }
 }
 
 inline static void pvr2_tex_mem_64bit_write_raw(struct pvr2_tex_mem *mem,
                                                 uint32_t addr, void const *srcp,
                                                 unsigned n_bytes) {
-    void *dstp = mem->tex64 + addr;
     if (addr + (n_bytes - 1) >= PVR2_TEX64_MEM_LEN) {
         error_set_address(addr);
         error_set_length(n_bytes);
         RAISE_ERROR(ERROR_INTEGRITY);
     }
-    memcpy(dstp, srcp, n_bytes * sizeof(uint8_t));
+
+    uint8_t const *src = (uint8_t*)srcp;
+    while (n_bytes) {
+        uint8_t val;
+        memcpy(&val, src, sizeof(val));
+        pvr2_tex_mem_64bit_write8(mem, addr, val);
+
+        --n_bytes;
+        ++addr;
+        ++src;
+    }
 }
 
 inline static void
@@ -318,7 +369,12 @@ pvr2_tex_mem_64bit_write_double(struct pvr2_tex_mem *mem,
         RAISE_ERROR(ERROR_INTEGRITY);
     }
 
-    memcpy(mem->tex64 + addr, &val, sizeof(val));
+    unsigned offs1 = pvr2_tex_mem_addr_64_to_32(addr);
+    unsigned offs2 = pvr2_tex_mem_addr_64_to_32(addr + 4);
+
+    memcpy(mem->tex32 + offs1, &val, sizeof(val)/2);
+    memcpy(mem->tex32 + offs2,
+           ((char*)&val) + sizeof(val)/2, sizeof(val)/2);
 }
 
 inline static void
@@ -331,7 +387,8 @@ pvr2_tex_mem_64bit_write_float(struct pvr2_tex_mem *mem,
         RAISE_ERROR(ERROR_INTEGRITY);
     }
 
-    memcpy(mem->tex64 + addr, &val, sizeof(val));
+    unsigned offs = pvr2_tex_mem_addr_64_to_32(addr);
+    memcpy(mem->tex32 + offs, &val, sizeof(val));
 }
 
 inline static void
@@ -344,7 +401,8 @@ pvr2_tex_mem_64bit_write32(struct pvr2_tex_mem *mem,
         RAISE_ERROR(ERROR_INTEGRITY);
     }
 
-    memcpy(mem->tex64 + addr, &val, sizeof(val));
+    unsigned offs = pvr2_tex_mem_addr_64_to_32(addr);
+    memcpy(mem->tex32 + offs, &val, sizeof(val));
 }
 
 inline static void
@@ -357,7 +415,8 @@ pvr2_tex_mem_64bit_write16(struct pvr2_tex_mem *mem,
         RAISE_ERROR(ERROR_INTEGRITY);
     }
 
-    memcpy(mem->tex64 + addr, &val, sizeof(val));
+    unsigned offs = pvr2_tex_mem_addr_64_to_32(addr);
+    memcpy(mem->tex32 + offs, &val, sizeof(val));
 }
 
 inline static void
@@ -370,7 +429,8 @@ pvr2_tex_mem_64bit_write8(struct pvr2_tex_mem *mem,
         RAISE_ERROR(ERROR_INTEGRITY);
     }
 
-    memcpy(mem->tex64 + addr, &val, sizeof(val));
+    unsigned offs = pvr2_tex_mem_addr_64_to_32(addr);
+    memcpy(mem->tex32 + offs, &val, sizeof(val));
 }
 
 extern struct memory_interface pvr2_tex_mem_area32_intf,
