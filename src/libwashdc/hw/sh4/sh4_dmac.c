1639f6204c32df62b3d1a7cc44ce7fc4863201ad
snickerbockers <snickerbockers@washemu.org>
2020-02-02 00:35:12 -0800

    dead macro removal

--- src/libwashdc/hw/sh4/sh4_dmac.c
+++ src/libwashdc/hw/sh4/sh4_dmac.c
@@ -38,9 +38,6 @@
 
 static void raise_ch2_dma_int_event_handler(struct SchedEvent *event);
 
-// this is arbitrary
-#define CH2_DMA_INT_DELAY 0
-
 struct SchedEvent raise_ch2_dma_int_event = {
     .handler = raise_ch2_dma_int_event_handler
 };
