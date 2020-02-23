fa48da7dc7035812d0541ed875ab3a4336679cc8
snickerbockers <snickerbockers@washemu.org>
2020-02-22 20:23:18 -0800

    sh4: partially implement UTLB/ITLB MMIO access via address and data arrays
    This  does not implement actual MMU functionality, it just lets programs read
    and write to the TLB when it's not enabled and feel like they're doing
    something.
    
    There are some games which don't use the MMU but nevertheless still clear the
    the valid bits on every entry.
    
    With this commit, the emulated Genesis games in SEGA Smash Pack are now
    in-game.  Virtua Cop and SEGA Swirl are still not in-game.  Also, Quake III
    Arena shows its intro FMV although it still doesn't get in-game or in-menu.

--- src/libwashdc/hw/sh4/sh4_mem.c
+++ src/libwashdc/hw/sh4/sh4_mem.c
@@ -2,7 +2,7 @@
  *
  *
  *    WashingtonDC Dreamcast Emulator
- *    Copyright (C) 2016-2019 snickerbockers
+ *    Copyright (C) 2016-2020 snickerbockers
  *
  *    This program is free software: you can redistribute it and/or modify
  *    it under the terms of the GNU General Public License as published by
@@ -27,6 +27,7 @@
 #include "hw/sh4/sh4_ocache.h"
 #include "hw/sh4/sh4_icache.h"
 #include "log.h"
+#include "intmath.h"
 
 #ifdef ENABLE_DEBUGGER
 #include "washdc/debugger.h"
@@ -55,6 +56,31 @@ static int sh4_try_write_p4_16(addr32_t addr, uint16_t val, void *ctxt);
 static int sh4_try_write_p4_8(addr32_t addr, uint8_t val, void *ctxt);
 static int sh4_try_write_p4_float(addr32_t addr, float val, void *ctxt);
 static int sh4_try_write_p4_double(addr32_t addr, double val, void *ctxt);
+
+static void
+sh4_utlb_addr_array_write(struct Sh4 *sh4, addr32_t addr, uint32_t val);
+static uint32_t
+sh4_utlb_addr_array_read(struct Sh4 *sh4, addr32_t addr);
+static void
+sh4_utlb_data_array_1_write(struct Sh4 *sh4, addr32_t addr, uint32_t val);
+static uint32_t
+sh4_utlb_data_array_1_read(struct Sh4 *sh4, addr32_t addr);
+static void
+sh4_utlb_data_array_2_write(struct Sh4 *sh4, addr32_t addr, uint32_t val);
+static uint32_t
+sh4_utlb_data_array_2_read(struct Sh4 *sh4, addr32_t addr);
+static void
+sh4_itlb_addr_array_write(struct Sh4 *sh4, addr32_t addr, uint32_t val);
+static uint32_t
+sh4_itlb_addr_array_read(struct Sh4 *sh4, addr32_t addr);
+static void
+sh4_itlb_data_array_1_write(struct Sh4 *sh4, addr32_t addr, uint32_t val);
+static uint32_t
+sh4_itlb_data_array_1_read(struct Sh4 *sh4, addr32_t addr);
+static void
+sh4_itlb_data_array_2_write(struct Sh4 *sh4, addr32_t addr, uint32_t val);
+static uint32_t
+sh4_itlb_data_array_2_read(struct Sh4 *sh4, addr32_t addr);
 
 struct memory_interface sh4_p4_intf = {
     .readdouble = sh4_do_read_p4_double,
@@ -109,6 +135,31 @@ void sh4_mem_cleanup(Sh4 *sh4) {
         } else if (addr >= SH4_IC_ADDR_ARRAY_FIRST &&                   \
                    addr <= SH4_IC_ADDR_ARRAY_LAST) {                    \
             sh4_icache_write_addr_array_##postfix(sh4, addr, val);      \
+        } else if (sizeof(val) == 4) {                                  \
+            if (addr >= SH4_P4_ITLB_ADDR_ARRAY_FIRST &&                 \
+                addr <= SH4_P4_ITLB_ADDR_ARRAY_LAST) {                  \
+                sh4_itlb_addr_array_write(sh4,  addr, val);             \
+            } else if (addr >= SH4_P4_ITLB_DATA_ARRAY_1_FIRST &&        \
+                       addr <= SH4_P4_ITLB_DATA_ARRAY_1_LAST) {         \
+                sh4_itlb_data_array_1_write(sh4, addr, val);            \
+            } else if (addr >= SH4_P4_ITLB_DATA_ARRAY_2_FIRST &&        \
+                       addr <= SH4_P4_ITLB_DATA_ARRAY_2_LAST) {         \
+                sh4_itlb_data_array_2_write(sh4, addr, val);            \
+            } else if (addr >= SH4_P4_UTLB_ADDR_ARRAY_FIRST &&          \
+                       addr <= SH4_P4_UTLB_ADDR_ARRAY_LAST) {           \
+                sh4_utlb_addr_array_write(sh4, addr, val);              \
+            } else if (addr >= SH4_P4_UTLB_DATA_ARRAY_1_FIRST &&        \
+                       addr <= SH4_P4_UTLB_DATA_ARRAY_1_LAST) {         \
+                sh4_utlb_data_array_1_write(sh4, addr, val);            \
+            } else if (addr >= SH4_P4_UTLB_DATA_ARRAY_2_FIRST &&        \
+                       addr <= SH4_P4_UTLB_DATA_ARRAY_2_LAST) {         \
+                sh4_utlb_data_array_2_write(sh4, addr, val);            \
+            } else {                                                    \
+                error_set_address(addr);                                \
+                error_set_length(sizeof(val));                          \
+                error_set_feature("writing to part of the P4 memory region"); \
+                RAISE_ERROR(ERROR_UNIMPLEMENTED);                       \
+            }                                                           \
         } else {                                                        \
             error_set_address(addr);                                    \
             error_set_length(sizeof(val));                              \
@@ -141,6 +192,34 @@ SH4_DO_WRITE_P4_TMPL(double, double)
                    addr <= SH4_IC_ADDR_ARRAY_LAST) {                    \
             sh4_icache_write_addr_array_##postfix(sh4, addr, val);      \
             return 0;                                                   \
+        } else if (sizeof(val) == 4) {                                  \
+            if (addr >= SH4_P4_ITLB_ADDR_ARRAY_FIRST &&                 \
+                addr <= SH4_P4_ITLB_ADDR_ARRAY_LAST) {                  \
+                sh4_itlb_addr_array_write(sh4,  addr, val);             \
+                return 0;                                               \
+            } else if (addr >= SH4_P4_ITLB_DATA_ARRAY_1_FIRST &&        \
+                       addr <= SH4_P4_ITLB_DATA_ARRAY_1_LAST) {         \
+                sh4_itlb_data_array_1_write(sh4, addr, val);            \
+                return 0;                                               \
+            } else if (addr >= SH4_P4_ITLB_DATA_ARRAY_2_FIRST &&        \
+                       addr <= SH4_P4_ITLB_DATA_ARRAY_2_LAST) {         \
+                sh4_itlb_data_array_2_write(sh4, addr, val);            \
+                return 0;                                               \
+            } else if (addr >= SH4_P4_UTLB_ADDR_ARRAY_FIRST &&          \
+                       addr <= SH4_P4_UTLB_ADDR_ARRAY_LAST) {           \
+                sh4_utlb_addr_array_write(sh4, addr, val);              \
+                return 0;                                               \
+            } else if (addr >= SH4_P4_UTLB_DATA_ARRAY_1_FIRST &&        \
+                       addr <= SH4_P4_UTLB_DATA_ARRAY_1_LAST) {         \
+                sh4_utlb_data_array_1_write(sh4, addr, val);            \
+                return 0;                                               \
+            } else if (addr >= SH4_P4_UTLB_DATA_ARRAY_2_FIRST &&        \
+                       addr <= SH4_P4_UTLB_DATA_ARRAY_2_LAST) {         \
+                sh4_utlb_data_array_2_write(sh4, addr, val);            \
+                return 0;                                               \
+            } else {                                                    \
+                return -1;                                              \
+            }                                                           \
         } else {                                                        \
             return -1;                                                  \
         }                                                               \
@@ -171,6 +250,31 @@ SH4_TRY_WRITE_P4_TMPL(double, double)
             LOG_INFO("UNKNOWN READ ADDRESS 0x%08x %u BYTES (PC=0x%08x)\n", \
                      (int)addr, (int)sizeof(type), (int)sh4->reg[SH4_REG_PC]); \
             return 0;                                                   \
+        } else if (sizeof(type) == 4) {                                 \
+            if (addr >= SH4_P4_ITLB_ADDR_ARRAY_FIRST &&                 \
+                addr <= SH4_P4_ITLB_ADDR_ARRAY_LAST) {                  \
+                return sh4_itlb_addr_array_read(sh4, addr);             \
+            } else if (addr >= SH4_P4_ITLB_DATA_ARRAY_1_FIRST &&        \
+                       addr <= SH4_P4_ITLB_DATA_ARRAY_1_LAST) {         \
+                return sh4_itlb_data_array_1_read(sh4, addr);           \
+            } else if (addr >= SH4_P4_ITLB_DATA_ARRAY_2_FIRST &&        \
+                       addr <= SH4_P4_ITLB_DATA_ARRAY_2_LAST) {         \
+                return sh4_itlb_data_array_2_read(sh4, addr);           \
+            } else if (addr >= SH4_P4_UTLB_ADDR_ARRAY_FIRST &&          \
+                       addr <= SH4_P4_UTLB_ADDR_ARRAY_LAST) {           \
+                return sh4_utlb_addr_array_read(sh4, addr);             \
+            } else if (addr >= SH4_P4_UTLB_DATA_ARRAY_1_FIRST &&        \
+                       addr <= SH4_P4_UTLB_DATA_ARRAY_1_LAST) {         \
+                return sh4_utlb_data_array_1_read(sh4, addr);           \
+            } else if (addr >= SH4_P4_UTLB_DATA_ARRAY_2_FIRST &&        \
+                       addr <= SH4_P4_UTLB_DATA_ARRAY_2_LAST) {         \
+                return sh4_utlb_data_array_2_read(sh4, addr);           \
+            } else {                                                    \
+                error_set_address(addr);                                \
+                error_set_length(sizeof(type));                         \
+                error_set_feature("writing to part of the P4 memory region"); \
+                RAISE_ERROR(ERROR_UNIMPLEMENTED);                       \
+            }                                                           \
         } else {                                                        \
             error_set_length(sizeof(type));                             \
             error_set_address(addr);                                    \
@@ -210,6 +314,34 @@ SH4_DO_READ_P4_TMPL(double, double)
             LOG_INFO("UNKNOWN READ ADDRESS 0x%08x %u BYTES PC=0x%08x\n", \
                      (int)addr, (int)sizeof(type), (int)sh4->reg[SH4_REG_PC]); \
             return 0;                                                   \
+        } else if (sizeof(type) == 4) {                                 \
+            if (addr >= SH4_P4_ITLB_ADDR_ARRAY_FIRST &&                 \
+                addr <= SH4_P4_ITLB_ADDR_ARRAY_LAST) {                  \
+                *valp = sh4_itlb_addr_array_read(sh4, addr);            \
+                return 0;                                               \
+            } else if (addr >= SH4_P4_ITLB_DATA_ARRAY_1_FIRST &&        \
+                       addr <= SH4_P4_ITLB_DATA_ARRAY_1_LAST) {         \
+                *valp = sh4_itlb_data_array_1_read(sh4, addr);          \
+                return 0;                                               \
+            } else if (addr >= SH4_P4_ITLB_DATA_ARRAY_2_FIRST &&        \
+                       addr <= SH4_P4_ITLB_DATA_ARRAY_2_LAST) {         \
+                *valp = sh4_itlb_data_array_2_read(sh4, addr);          \
+                return 0;                                               \
+            } else if (addr >= SH4_P4_UTLB_ADDR_ARRAY_FIRST &&          \
+                       addr <= SH4_P4_UTLB_ADDR_ARRAY_LAST) {           \
+                *valp = sh4_utlb_addr_array_read(sh4, addr);            \
+                return 0;                                               \
+            } else if (addr >= SH4_P4_UTLB_DATA_ARRAY_1_FIRST &&        \
+                       addr <= SH4_P4_UTLB_DATA_ARRAY_1_LAST) {         \
+                *valp = sh4_utlb_data_array_1_read(sh4, addr);          \
+                return 0;                                               \
+            } else if (addr >= SH4_P4_UTLB_DATA_ARRAY_2_FIRST &&        \
+                       addr <= SH4_P4_UTLB_DATA_ARRAY_2_LAST) {         \
+                *valp = sh4_utlb_data_array_2_read(sh4, addr);          \
+                return 0;                                               \
+            } else {                                                    \
+                return -1;                                              \
+            }                                                           \
         } else {                                                        \
             return -1;                                                  \
         }                                                               \
@@ -221,6 +353,196 @@ SH4_TRY_READ_P4_TMPL(uint32_t, 32)
 SH4_TRY_READ_P4_TMPL(float, float)
 SH4_TRY_READ_P4_TMPL(double, double)
 
+static uint32_t vpn_mask_for_size(enum sh4_tlb_page_sz sz) {
+    switch (sz) {
+    case SH4_TLB_PAGE_1KB:
+        return ~0x3ff;
+    case SH4_TLB_PAGE_4KB:
+        return ~0xfff;
+    case SH4_TLB_PAGE_64KB:
+        return ~0xffff;
+    case SH4_TLB_PAGE_1MB:
+        return ~0xfffff;
+    default:
+        RAISE_ERROR(ERROR_INTEGRITY);
+    }
+}
+
+// vpn should be shifted such that the MSB is at bit 31
+static struct sh4_utlb_ent *
+sh4_utlb_find_ent_associative(struct Sh4 *sh4, uint32_t vpn) {
+    struct sh4_utlb_ent *ent = NULL;
+    unsigned asid = sh4->reg[SH4_REG_PTEH] & BIT_RANGE(0, 7);
+    unsigned idx;
+    for (idx = 0; idx < SH4_UTLB_LEN; idx++) {
+        struct sh4_utlb_ent *curs = sh4->mem.utlb + idx;
+        uint32_t mask = vpn_mask_for_size(curs->sz);
+        if ((curs->vpn & mask) == (vpn & mask) && curs->asid == asid) {
+            if (ent) {
+                error_set_feature("UTLB multiple hit exception");
+                RAISE_ERROR(ERROR_UNIMPLEMENTED);
+            }
+            ent = curs;
+            return ent;
+        }
+    }
+    return NULL;
+}
+
+// vpn should be shifted such that the MSB is at bit 31
+static struct sh4_itlb_ent *
+sh4_itlb_find_ent_associative(struct Sh4 *sh4, uint32_t vpn) {
+    struct sh4_itlb_ent *ent = NULL;
+    unsigned asid = sh4->reg[SH4_REG_PTEH] & BIT_RANGE(0, 7);
+    unsigned idx;
+    for (idx = 0; idx < SH4_ITLB_LEN; idx++) {
+        struct sh4_itlb_ent *curs = sh4->mem.itlb + idx;
+        uint32_t mask = vpn_mask_for_size(curs->sz);
+        if ((curs->vpn & mask) == (vpn & mask) && curs->asid == asid) {
+            if (ent) {
+                error_set_feature("ITLB multiple hit exception");
+                RAISE_ERROR(ERROR_UNIMPLEMENTED);
+            }
+            ent = curs;
+            return ent;
+        }
+    }
+    return NULL;
+}
+
 void sh4_set_mem_map(struct Sh4 *sh4, struct memory_map *map) {
     sh4->mem.map = map;
+}
+
+static void sh4_utlb_addr_array_write(struct Sh4 *sh4, addr32_t addr, uint32_t val) {
+    struct sh4_utlb_ent *ent;
+    bool associative = (addr >> 7) & 1 ? true : false;
+    bool valid = (val >> 8) & 1 ? true : false;
+    bool dirty = (val >> 9) & 1 ? true : false;
+
+    uint32_t vpn = (val & BIT_RANGE(10, 31)) >> 10;
+    uint32_t asid = val & BIT_RANGE(0, 7);
+
+    if (associative)
+        LOG_WARN("UTLB ADDRESS ARRAY ASSOCIATIVE WRITE %08X to %08X\n",
+                 (unsigned)val, (unsigned)addr);
+    else
+        LOG_WARN("UTLB ADDRESS ARRAY NON-ASSOCIATIVE WRITE %08X to %08X\n",
+                 (unsigned)val, (unsigned)addr);
+
+    if (associative) {
+        /*
+         * XXX SH4 spec is pretty vague about how this UTLB->ITLB propagation
+         * works, so some of this may be wrong.  If I understand correctly, then
+         * the valid bit gets copied over if it matches the ITLB but not the
+         * UTLB...and then if the UTLB matches too then it copies else everything over as well?
+         *
+         * I feel like there's an MMU hardware test coming...
+         */
+        struct sh4_itlb_ent *itlb_ent = sh4_itlb_find_ent_associative(sh4, vpn);
+        if (itlb_ent)
+            itlb_ent->valid = valid;
+
+        if (!(ent = sh4_utlb_find_ent_associative(sh4, vpn)))
+            return;
+
+        ent->valid = valid;
+        ent->dirty = dirty;
+
+        if (itlb_ent) {
+            itlb_ent->asid = ent->asid;
+            itlb_ent->vpn = ent->vpn;
+            itlb_ent->ppn = ent->ppn;
+            itlb_ent->protection = ent->protection & 2;
+            itlb_ent->sa = ent->sa;
+            itlb_ent->sz = ent->sz;
+            itlb_ent->shared = ent->shared;
+            itlb_ent->cacheable = ent->cacheable;
+            itlb_ent->tc = ent->tc;
+        }
+    } else {
+        unsigned idx = (addr >> 8) & 0x3f;
+        ent = sh4->mem.utlb + idx;
+
+        ent->vpn = vpn;
+        ent->asid = asid;
+        ent->dirty = dirty;
+        ent->valid = valid;
+    }
+}
+
+static uint32_t sh4_utlb_addr_array_read(struct Sh4 *sh4, addr32_t addr) {
+    // associative access is never performed for reads
+    unsigned idx = (addr >> 8) & 0x3f;
+    struct sh4_utlb_ent *ent = sh4->mem.utlb + idx;
+    return (ent->vpn << 10) | (ent->dirty << 9) |
+        (ent->valid << 8) | (ent->asid & BIT_RANGE(0,7));
+}
+
+static void
+sh4_utlb_data_array_1_write(struct Sh4 *sh4, addr32_t addr, uint32_t val) {
+    LOG_WARN("UTLB DATA ARRAY 1 WRITE %08X to %08X\n",
+             (unsigned)val, (unsigned)addr);
+
+    unsigned idx = (addr >> 8) & 0x3f;
+    struct sh4_utlb_ent *ent = sh4->mem.utlb + idx;
+
+    unsigned ppn = (val & BIT_RANGE(10, 28)) >> 10;
+    enum sh4_tlb_page_sz sz =
+        (enum sh4_tlb_page_sz)(((val >> 4) & 1) | ((val >> 6) & 2));
+    bool valid = (val >> 8) & 1;
+    unsigned protection = (val >> 5) & 3;
+    bool cacheable = (val >> 3) & 1;
+    bool dirty = (val >> 2) & 1;
+    bool shared = (val >> 1) & 1;
+    bool wt = val & 1;
+
+    ent->ppn = ppn;
+    ent->sz = sz;
+    ent->valid = valid;
+    ent->protection = protection;
+    ent->cacheable = cacheable;
+    ent->dirty = dirty;
+    ent->shared = shared;
+    ent->wt = wt;
+}
+
+static uint32_t sh4_utlb_data_array_1_read(struct Sh4 *sh4, addr32_t addr) {
+    RAISE_ERROR(ERROR_UNIMPLEMENTED);
+}
+
+static void
+sh4_utlb_data_array_2_write(struct Sh4 *sh4, addr32_t addr, uint32_t val) {
+    RAISE_ERROR(ERROR_UNIMPLEMENTED);
+}
+
+static uint32_t sh4_utlb_data_array_2_read(struct Sh4 *sh4, addr32_t addr) {
+    RAISE_ERROR(ERROR_UNIMPLEMENTED);
+}
+
+static void
+sh4_itlb_addr_array_write(struct Sh4 *sh4, addr32_t addr, uint32_t val) {
+    RAISE_ERROR(ERROR_UNIMPLEMENTED);
+}
+
+static uint32_t sh4_itlb_addr_array_read(struct Sh4 *sh4, addr32_t addr) {
+    RAISE_ERROR(ERROR_UNIMPLEMENTED);
+}
+
+static void
+sh4_itlb_data_array_1_write(struct Sh4 *sh4, addr32_t addr, uint32_t val) {
+    RAISE_ERROR(ERROR_UNIMPLEMENTED);
+}
+
+static uint32_t sh4_itlb_data_array_1_read(struct Sh4 *sh4, addr32_t addr) {
+    RAISE_ERROR(ERROR_UNIMPLEMENTED);
+}
+
+static void
+sh4_itlb_data_array_2_write(struct Sh4 *sh4, addr32_t addr, uint32_t val) {
+    RAISE_ERROR(ERROR_UNIMPLEMENTED);
+}
+
+static uint32_t sh4_itlb_data_array_2_read(struct Sh4 *sh4, addr32_t addr) {
+    RAISE_ERROR(ERROR_UNIMPLEMENTED);
 }
--- src/libwashdc/hw/sh4/sh4_mem.h
+++ src/libwashdc/hw/sh4/sh4_mem.h
@@ -2,7 +2,7 @@
  *
  *
  *    WashingtonDC Dreamcast Emulator
- *    Copyright (C) 2017-2019 snickerbockers
+ *    Copyright (C) 2017-2020 snickerbockers
  *
  *    This program is free software: you can redistribute it and/or modify
  *    it under the terms of the GNU General Public License as published by
@@ -24,6 +24,7 @@
 #define SH4_MEM_H_
 
 #include <assert.h>
+#include <stdbool.h>
 
 #include "sh4_excp.h"
 #include "mem_code.h"
@@ -70,6 +71,19 @@ static_assert((SH4_P4_REGEND - SH4_P4_REGSTART) ==
               (SH4_AREA7_REGEND - SH4_AREA7_REGSTART),
               "AREA7 is not the same size as the P4 area");
 
+#define SH4_P4_ITLB_ADDR_ARRAY_FIRST   0xf2000000
+#define SH4_P4_ITLB_ADDR_ARRAY_LAST    0xf2ffffff
+#define SH4_P4_ITLB_DATA_ARRAY_1_FIRST 0xf3000000
+#define SH4_P4_ITLB_DATA_ARRAY_1_LAST  0xf37fffff
+#define SH4_P4_ITLB_DATA_ARRAY_2_FIRST 0xf3800000
+#define SH4_P4_ITLB_DATA_ARRAY_2_LAST  0xf3ffffff
+#define SH4_P4_UTLB_ADDR_ARRAY_FIRST   0xf6000000
+#define SH4_P4_UTLB_ADDR_ARRAY_LAST    0xf6ffffff
+#define SH4_P4_UTLB_DATA_ARRAY_1_FIRST 0xf7000000
+#define SH4_P4_UTLB_DATA_ARRAY_1_LAST  0xf77fffff
+#define SH4_P4_UTLB_DATA_ARRAY_2_FIRST 0xf7800000
+#define SH4_P4_UTLB_DATA_ARRAY_2_LAST  0xf77fffff
+
 /* constants needed for opcache as ram */
 #define SH4_LONGS_PER_OP_CACHE_LINE 8
 #define SH4_OP_CACHE_LINE_SIZE (SH4_LONGS_PER_OP_CACHE_LINE * 4)
@@ -78,8 +92,78 @@ static_assert((SH4_P4_REGEND - SH4_P4_REGSTART) ==
 void sh4_mem_init(Sh4 *sh4);
 void sh4_mem_cleanup(Sh4 *sh4);
 
+#define SH4_UTLB_LEN 64
+#define SH4_ITLB_LEN 4
+
+enum sh4_tlb_page_sz {
+    SH4_TLB_PAGE_1KB = 0,
+    SH4_TLB_PAGE_4KB = 1,
+    SH4_TLB_PAGE_64KB = 2,
+    SH4_TLB_PAGE_1MB = 3
+};
+
+struct sh4_utlb_ent {
+    unsigned asid : 8;
+    unsigned vpn : 22;
+    unsigned ppn : 19;
+
+    /*
+     * 2 bits
+     * bit 0 is set if writable, cleared for read-only.
+     * bit 1 is set if accessible in user-mode, cleared for privileged-only.
+     */
+    unsigned protection : 2;
+
+    /*
+     * space attribute (three bits)
+     *
+     * Has somethhing to do with PCMIA, we don't actually care about this.
+     */
+    unsigned sa : 3;
+
+    enum sh4_tlb_page_sz sz : 2;
+
+    bool valid : 1;
+    bool shared : 1;
+    bool cacheable : 1;
+    bool dirty : 1;
+    bool wt : 1; //write-through
+    bool tc : 1; // i sincerely hope i never need to understand what this is.
+};
+
+struct sh4_itlb_ent {
+    unsigned asid : 8;
+    unsigned vpn : 22;
+    unsigned ppn : 19;
+
+    /*
+     * only 1 bit, unlike for utlb
+     * bit 0 is set if accessible in user-mode, cleared for privileged-only.
+     *
+     * I guess it doesn't make sense to have a writable bit in the itlb.
+     */
+    unsigned protection : 1;
+
+    /*
+     * space attribute (three bits)
+     *
+     * Has somethhing to do with PCMIA, we don't actually care about this.
+     */
+    unsigned sa : 3;
+
+    enum sh4_tlb_page_sz sz : 2;
+
+    bool valid : 1;
+    bool shared : 1;
+    bool cacheable : 1;
+    bool tc : 1; // i sincerely hope i never need to understand what this is.
+};
+
 struct sh4_mem {
     struct memory_map *map;
+
+    struct sh4_utlb_ent utlb[SH4_UTLB_LEN];
+    struct sh4_itlb_ent itlb[SH4_ITLB_LEN];
 };
 
 void sh4_set_mem_map(struct Sh4 *sh4, struct memory_map *map);
