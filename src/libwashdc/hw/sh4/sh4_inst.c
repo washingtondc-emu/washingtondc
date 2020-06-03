washingtondc-emu/washingtondc

commit 5259f753f808b9ccff4fe4aa71015d186e0aeb4f

sh4: implement FMOV XDm, @Rn
With this commit, Spider-Man is now in-game.

src/libwashdc/hw/sh4/sh4_inst.c
@@ -6937,10 +6937,12 @@ void sh4_inst_binary_fmov_xd_indgen(void *cpu, cpu_inst_param inst) {
 
     CHECK_FPSCR(sh4->reg[SH4_REG_FPSCR], SH4_FPSCR_SZ_MASK, SH4_FPSCR_SZ_MASK);
 
-    error_set_feature("opcode implementation");
-    error_set_opcode_format("1111nnnnmmm11010");
-    error_set_opcode_name("FMOV XDm, @Rn");
-    SH4_INST_RAISE_ERROR(sh4, ERROR_UNIMPLEMENTED);
+    reg32_t addr = *sh4_gen_reg(sh4, (inst >> 8) & 0xf);
+    double src_val = *sh4_fpu_xd(sh4, (inst >> 5) & 7);
+
+    if (sh4_writedouble(sh4, addr, src_val) != 0) {
+        // nothing to do here
+    }
 }
 
 #define INST_MASK_1111nnnnmmm11011 0xf01f

commit f2601f7c875e98100731385821f0cfce3d5e1043

fix build failure introduced by 5259f753f808b9ccff4fe4aa71015d186e0aeb4f
It only failed if you didn't have INVARIANTS or ENABLE_MMU enabled, which is
why I didn't notice before I pushed that commit out to github.

src/libwashdc/hw/sh4/sh4_inst.c
@@ -6926,9 +6926,7 @@ void sh4_inst_binary_fmov_xd_indgen(void *cpu, cpu_inst_param inst) {
 
     CHECK_INST(inst, INST_MASK_1111nnnnmmm11010, INST_CONS_1111nnnnmmm11010);
 
-#if defined(INVARIANTS) || defined(ENABLE_MMU)
     struct Sh4 *sh4 = (struct Sh4*)cpu;
-#endif
 
 #ifdef ENABLE_MMU
     if (sh4_check_fpu_disabled(sh4) != 0)