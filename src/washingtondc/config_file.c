washingtondc-emu/washingtondc

commit b2e0fe72012a957f367dac34ebd97c20fc6fb5e4

moving configuration file parsing into the frontend

src/washingtondc/config_file.c
@@ -39,11 +39,9 @@
 #include <errno.h>
 #include <sys/stat.h>
 
-#include "log.h"
 #include "washdc/fifo.h"
-#include "washdc/hostfile.h"
 
-#include "washdc/config_file.h"
+#include "config_file.h"
 
 #define CFG_NODE_KEY_LEN 256
 #define CFG_NODE_VAL_LEN 256
@@ -77,7 +75,7 @@ static void cfg_add_entry(void);
 static void cfg_handle_newline(void);
 static int cfg_parse_bool(char const *val, bool *outp);
 
-static void cfg_create_default_config(void) {
+void cfg_create_default_config(FILE *cfg_file) {
     static char const *cfg_default =
         ";;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;"
         ";;;;;;;;;;\n"
@@ -427,51 +425,24 @@ static void cfg_create_default_config(void) {
         "dc.ctrl.p1_1.kbd-us.s2 kbd.rsuper\n"
         ;
 
-    washdc_hostfile cfg_file =
-        washdc_hostfile_open_cfg_file(WASHDC_HOSTFILE_WRITE |
-                                      WASHDC_HOSTFILE_TEXT);
-    if (cfg_file == WASHDC_HOSTFILE_INVALID) {
-        LOG_ERROR("unable to open cfg file\n");
-        return;
-    }
-
-    if (washdc_hostfile_puts(cfg_file, cfg_default) == WASHDC_HOSTFILE_EOF) {
-        LOG_ERROR("Unable to write default config to cfg file\n");
-    }
-
-    washdc_hostfile_close(cfg_file);
+    fputs(cfg_default, cfg_file);
 }
 
-void cfg_init(void) {
+void cfg_init(FILE *cfg_file) {
     memset(&cfg_state, 0, sizeof(cfg_state));
     cfg_state.state = CFG_PARSE_PRE_KEY;
 
     fifo_init(&cfg_state.cfg_nodes);
 
-    washdc_hostfile cfg_file =
-        washdc_hostfile_open_cfg_file(WASHDC_HOSTFILE_READ |
-                                      WASHDC_HOSTFILE_TEXT);
-
-    if (cfg_file == WASHDC_HOSTFILE_INVALID) {
-        cfg_create_default_config();
-        cfg_file =
-            washdc_hostfile_open_cfg_file(WASHDC_HOSTFILE_READ |
-                                          WASHDC_HOSTFILE_TEXT);
+    printf("Parsing wash.cfg\n");
+    for (;;) {
+        int ch = fgetc(cfg_file);
+        if (ch == EOF)
+            break;
+        cfg_put_char(ch);
     }
-
-    if (cfg_file != WASHDC_HOSTFILE_INVALID) {
-        LOG_INFO("Parsing wash.cfg\n");
-        for (;;) {
-            int ch = washdc_hostfile_getc(cfg_file);
-            if (ch == WASHDC_HOSTFILE_EOF)
-                break;
-            cfg_put_char(ch);
-        }
-        cfg_put_char('\n'); // in case the last line doesn't end with newline
-        washdc_hostfile_close(cfg_file);
-    } else {
-        LOG_INFO("Unable to open wash.cfg; does it even exist?\n");
-    }
+    cfg_put_char('\n'); // in case the last line doesn't end with newline
+    fclose(cfg_file);
 }
 
 void cfg_cleanup(void) {
@@ -516,7 +487,7 @@ void cfg_put_char(char ch) {
         break;
     case CFG_PARSE_KEY:
         if (ch == '\n') {
-            LOG_ERROR("*** CFG ERROR INCOMPLETE LINE %u ***\n", cfg_state.line_count);
+            fprintf(stderr, "*** CFG ERROR INCOMPLETE LINE %u ***\n", cfg_state.line_count);
             cfg_handle_newline();
         } else if (isspace(ch)) {
             cfg_state.state = CFG_PARSE_PRE_VAL;
@@ -524,14 +495,14 @@ void cfg_put_char(char ch) {
         } else if (cfg_state.key_len < CFG_NODE_KEY_LEN - 1) {
             cfg_state.key[cfg_state.key_len++] = ch;
         } else {
-            LOG_WARN("CFG file dropped char from line %u; key length is "
-                     "limited to %u characters\n",
-                     cfg_state.line_count, CFG_NODE_KEY_LEN - 1);
+            printf("CFG file dropped char from line %u; key length is "
+                   "limited to %u characters\n",
+                   cfg_state.line_count, CFG_NODE_KEY_LEN - 1);
         }
         break;
     case CFG_PARSE_PRE_VAL:
         if (ch == '\n') {
-            LOG_ERROR("*** CFG ERROR INCOMPLETE LINE %u ***\n", cfg_state.line_count);
+            fprintf(stderr, "*** CFG ERROR INCOMPLETE LINE %u ***\n", cfg_state.line_count);
             cfg_handle_newline();
         } else if (!isspace(ch)) {
             cfg_state.state = CFG_PARSE_VAL;
@@ -550,9 +521,9 @@ void cfg_put_char(char ch) {
         } else if (cfg_state.val_len < CFG_NODE_VAL_LEN - 1) {
             cfg_state.val[cfg_state.val_len++] = ch;
         } else {
-            LOG_WARN("CFG file dropped char from line %u; value length is "
-                     "limited to %u characters\n",
-                     cfg_state.line_count, CFG_NODE_VAL_LEN - 1);
+            printf("CFG file dropped char from line %u; value length is "
+                   "limited to %u characters\n",
+                   cfg_state.line_count, CFG_NODE_VAL_LEN - 1);
         }
         break;
     case CFG_PARSE_POST_VAL:
@@ -561,7 +532,7 @@ void cfg_put_char(char ch) {
             cfg_handle_newline();
         } else if (!isspace(ch)) {
             cfg_state.state = CFG_PARSE_ERROR;
-            LOG_ERROR("*** CFG ERROR INVALID DATA LINE %u ***\n", cfg_state.line_count);
+            fprintf(stderr, "*** CFG ERROR INVALID DATA LINE %u ***\n", cfg_state.line_count);
         }
         break;
     default:
@@ -585,10 +556,10 @@ static void cfg_add_entry(void) {
     }
 
     if (dst_node) {
-        LOG_INFO("CFG overwriting existing config key \"%s\" at line %u\n",
+        printf("CFG overwriting existing config key \"%s\" at line %u\n",
                  cfg_state.key, cfg_state.line_count);
     } else {
-        LOG_INFO("CFG allocating new config key \"%s\" at line %u\n",
+        printf("CFG allocating new config key \"%s\" at line %u\n",
                  cfg_state.key, cfg_state.line_count);
         dst_node = (struct cfg_node*)malloc(sizeof(struct cfg_node));
         memcpy(dst_node->key, cfg_state.key, sizeof(dst_node->key));
@@ -598,7 +569,7 @@ static void cfg_add_entry(void) {
     if (dst_node)
         memcpy(dst_node->val, cfg_state.val, sizeof(dst_node->val));
     else
-        LOG_ERROR("CFG file dropped line %u due to failed node allocation\n", cfg_state.line_count);
+        fprintf(stderr, "CFG file dropped line %u due to failed node allocation\n", cfg_state.line_count);
 }
 
 static void cfg_handle_newline(void) {
@@ -636,7 +607,7 @@ int cfg_get_bool(char const *key, bool *outp) {
     if (nodestr) {
         int success = cfg_parse_bool(nodestr, outp);
         if (success != 0)
-            LOG_ERROR("error parsing config node \"%s\"\n", key);
+            fprintf(stderr, "error parsing config node \"%s\"\n", key);
         return success;
     }
     return -1;
@@ -661,7 +632,7 @@ static int cfg_parse_rgb(char const *valstr, int *red, int *green, int *blue) {
         } else if (ch >= 'A' && ch <= 'F') {
             digits[idx] = ch - 'A' + 10;
         } else {
-            LOG_ERROR("Bad color syntax \"%s\"\n", valstr);
+            fprintf(stderr, "Bad color syntax \"%s\"\n", valstr);
             return -1;
         }
     }
@@ -710,7 +681,7 @@ int cfg_get_rgb(char const *key, int *red, int *green, int *blue) {
     if (nodestr) {
         int success = cfg_parse_rgb(nodestr, red, green, blue);
         if (success != 0)
-            LOG_ERROR("error parsing config node \"%s\"\n", key);
+            fprintf(stderr, "error parsing config node \"%s\"\n", key);
         return success;
     }
     return -1;
@@ -722,7 +693,7 @@ int cfg_get_int(char const *key, int *val) {
     if (nodestr) {
         int success = cfg_parse_decimal_int(nodestr, val);
         if (success != 0)
-            LOG_ERROR("error parsing config node \"%s\"\n", key);
+            fprintf(stderr, "error parsing config node \"%s\"\n", key);
         return success;
     }
     return -1;

src/libwashdc/CMakeLists.txt
@@ -240,8 +240,6 @@ set(libwashdc_sources "${WASHDC_SOURCE_DIR}/hw/sh4/sh4.c"
                       "${WASHDC_SOURCE_DIR}/title.h"
                       "${WASHDC_SOURCE_DIR}/title.c"
                       "${WASHDC_SOURCE_DIR}/include/washdc/cpu.h"
-                      "${WASHDC_SOURCE_DIR}/include/washdc/config_file.h"
-                      "${WASHDC_SOURCE_DIR}/config_file.c"
                       "${WASHDC_SOURCE_DIR}/include/washdc/hostfile.h"
                       "${WASHDC_SOURCE_DIR}/screenshot.h"
                       "${WASHDC_SOURCE_DIR}/screenshot.c"

src/libwashdc/config.c
@@ -104,3 +104,5 @@ CONFIG_DEF_BOOL(inline_mem, true);
 
 CONFIG_DEF_BOOL(log_verbose, false);
 CONFIG_DEF_BOOL(log_stdout, false);
+
+CONFIG_DEF_BOOL(dump_mem_on_error, false)

src/libwashdc/config.h
@@ -115,4 +115,6 @@ CONFIG_DECL_BOOL(inline_mem);
 CONFIG_DECL_BOOL(log_stdout);
 CONFIG_DECL_BOOL(log_verbose);
 
+CONFIG_DECL_BOOL(dump_mem_on_error);
+
 #endif

src/libwashdc/dreamcast.c
@@ -87,7 +87,6 @@
 #include "hw/boot_rom.h"
 #include "hw/arm7/arm7.h"
 #include "title.h"
-#include "washdc/config_file.h"
 #include "mount.h"
 #include "gdi.h"
 #include "cdi.h"
@@ -508,8 +507,8 @@ dreamcast_init(char const *gdi_path,
                struct debug_frontend const *dbg_frontend,
                struct serial_server_intf const *ser_intf,
                struct washdc_sound_intf const *snd_intf,
-               bool flash_mem_writeable) {
-    int win_width, win_height;
+               bool flash_mem_writeable,
+               char const *controllers[4][3]) {
 
     frame_count = 0;
 
@@ -556,19 +555,17 @@ dreamcast_init(char const *gdi_path,
 
     title_set_content(title_content);
 
-    cfg_init();
-
     washdc_atomic_int_init(&signal_exit_threads, 0);
     washdc_atomic_int_init(&is_running, 1);
 
-    if (cfg_get_int("win.external-res.x", &win_width) != 0 || win_width <= 0)
-        win_width = 640;
-    if (cfg_get_int("win.external-res.y", &win_height) != 0 || win_height <= 0)
-        win_height = 480;
+    /* if (cfg_get_int("win.external-res.x", &win_width) != 0 || win_width <= 0) */
+    /*     win_width = 640; */
+    /* if (cfg_get_int("win.external-res.y", &win_height) != 0 || win_height <= 0) */
+    /*     win_height = 480; */
 
     // initialize host windowing, graphics and sound systems
-    win_init(win_width, win_height);
-    gfx_init(gfx_if, win_width, win_height);
+    /* win_init(win_width, win_height); */
+    gfx_init(gfx_if);
     dc_sound_init(snd_intf);
 
     memory_init(&dc_mem);
@@ -652,47 +649,15 @@ dreamcast_init(char const *gdi_path,
     gdrom_init(&gdrom, &sh4_clock);
     maple_init(&maple, &sh4_clock);
 
-    char const *ctrl_0 = cfg_get_node("wash.dc.port.0.0");
-    char const *ctrl_1 = cfg_get_node("wash.dc.port.1.0");
-    char const *ctrl_2 = cfg_get_node("wash.dc.port.2.0");
-    char const *ctrl_3 = cfg_get_node("wash.dc.port.3.0");
-
-    if (ctrl_0) {
-        if (strcmp(ctrl_0, "dreamcast_controller") == 0) {
-            maple_device_init(&maple, maple_addr_pack(0, 0),
+    unsigned port;
+    for (port = 0; port < 4; port++)
+        if (strcmp(controllers[port][0], "dreamcast_controller") == 0) {
+            maple_device_init(&maple, maple_addr_pack(port, 0),
                               MAPLE_DEVICE_CONTROLLER);
-        } else if (strcmp(ctrl_0, "dreamcast_keyboard_us") == 0) {
-            maple_device_init(&maple, maple_addr_pack(0, 0),
+        } else if (strcmp(controllers[port][0], "dreamcast_keyboard_us") == 0) {
+            maple_device_init(&maple, maple_addr_pack(port, 0),
                               MAPLE_DEVICE_KEYBOARD);
         }
-    }
-    if (ctrl_1) {
-        if (strcmp(ctrl_1, "dreamcast_controller") == 0) {
-            maple_device_init(&maple, maple_addr_pack(1, 0),
-                              MAPLE_DEVICE_CONTROLLER);
-        } else if (strcmp(ctrl_1, "dreamcast_keyboard_us") == 0) {
-            maple_device_init(&maple, maple_addr_pack(1, 0),
-                              MAPLE_DEVICE_KEYBOARD);
-        }
-    }
-    if (ctrl_2) {
-        if (strcmp(ctrl_2, "dreamcast_controller") == 0) {
-            maple_device_init(&maple, maple_addr_pack(2, 0),
-                              MAPLE_DEVICE_CONTROLLER);
-        } else if (strcmp(ctrl_0, "dreamcast_keyboard_us") == 0) {
-            maple_device_init(&maple, maple_addr_pack(2, 0),
-                              MAPLE_DEVICE_KEYBOARD);
-        }
-    }
-    if (ctrl_3) {
-        if (strcmp(ctrl_3, "dreamcast_controller") == 0) {
-            maple_device_init(&maple, maple_addr_pack(3, 0),
-                              MAPLE_DEVICE_CONTROLLER);
-        } else if (strcmp(ctrl_3, "dreamcast_keyboard_us") == 0) {
-            maple_device_init(&maple, maple_addr_pack(3, 0),
-                              MAPLE_DEVICE_KEYBOARD);
-        }
-    }
 
     // hook up the irl line
     sh4_register_irl_line(&cpu, holly_intc_irl_line_fn, NULL);
@@ -817,9 +782,7 @@ void dreamcast_cleanup() {
 
     dc_sound_cleanup();
     gfx_cleanup();
-    win_cleanup();
-
-    cfg_cleanup();
+    /* win_cleanup(); */
 
     if (mount_check())
         mount_eject();

src/libwashdc/dreamcast.h
@@ -70,7 +70,8 @@ dreamcast_init(char const *gdi_path,
                struct debug_frontend const *dbg_frontend,
                struct serial_server_intf const *ser_intf,
                struct washdc_sound_intf const *snd_intf,
-               bool flash_mem_writeable);
+               bool flash_mem_writeable,
+               char const *controllers[4][3]);
 
 void dreamcast_cleanup();
 

src/libwashdc/error.c
@@ -38,7 +38,6 @@
 #include "washdc/fifo.h"
 #include "dreamcast.h"
 #include "log.h"
-#include "washdc/config_file.h"
 
 #include "washdc/error.h"
 
@@ -70,7 +69,7 @@ WASHDC_NORETURN void error_raise(enum error_type tp) {
     error_print();
 
     bool dump_mem;
-    if (cfg_get_bool("wash.dbg.dump_mem_on_error", &dump_mem) == 0 && dump_mem)
+    if (config_get_dump_mem_on_error())
         washdc_dump_main_memory("washdc_error_dump.bin");
 
     fflush(stdout);

src/libwashdc/gfx/gfx.c
@@ -47,15 +47,10 @@
 
 #include "gfx/gfx.h"
 
-static unsigned win_width, win_height;
-
 // Only call gfx_thread_signal and gfx_thread_wait when you hold the lock.
 static void gfx_do_init(struct gfx_rend_if const * rend_if);
 
-void gfx_init(struct gfx_rend_if const * rend_if, unsigned width, unsigned height) {
-    win_width = width;
-    win_height = height;
-
+void gfx_init(struct gfx_rend_if const * rend_if) {
     LOG_INFO("GFX: rendering graphics from within the main emulation thread\n");
     gfx_do_init(rend_if);
 }

commit e80ced71b31149381d49bf1d89413f0c0dfe48f0

cfg_file: fix double fclose
this fixes the problem of the emulator hanging at initialization on Linux.

this bug was introduced by commit b2e0fe72012a957f367dac34ebd97c20fc6fb5e4

src/washingtondc/config_file.c
@@ -442,7 +442,6 @@ void cfg_init(FILE *cfg_file) {
         cfg_put_char(ch);
     }
     cfg_put_char('\n'); // in case the last line doesn't end with newline
-    fclose(cfg_file);
 }
 
 void cfg_cleanup(void) {