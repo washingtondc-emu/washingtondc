washingtondc-emu/washingtondc

commit fb87e96dba12087b515c15692e27d27d6045dc5b

preliminary support for PuruPuru vibration emulation
it's only in the emulation code though, there's nothing hooking it up to the
frontend to actually vibrate the controller.  This is due in part to GLFW's
lack of support for force-feedback, and in part due to my lack of information
on how to decode the 4-byte value in the maplebus setcond packets that tells
the PuruPuru how to vibrate.

So for now it's disabled by default and if you do enable it then all it does is
printf to stdout when one of the controllers should be vibrating.

src/libwashdc/hw/maple/maple.c
@@ -95,9 +95,15 @@ static void maple_dma_complete(struct maple *ctxt);
 
 static void maple_handle_devinfo(struct maple *ctxt, struct maple_frame *frame);
 static void maple_handle_getcond(struct maple *ctxt, struct maple_frame *frame);
+static void maple_handle_bwrite(struct maple *ctxt, struct maple_frame *frame);
+static void maple_handle_setcond(struct maple *ctxt, struct maple_frame *frame);
 
 static void maple_decode_frame(struct maple_frame *frame_out,
                                uint32_t const dat[3]);
+
+static void
+maple_write_frame_resp(struct maple *ctxt,
+                       struct maple_frame *frame, unsigned resp_code);
 
 static DEF_ERROR_INT_ATTR(maple_command);
 
@@ -133,7 +139,13 @@ static void maple_handle_frame(struct maple *ctxt, struct maple_frame *frame) {
     case MAPLE_CMD_GETCOND:
         maple_handle_getcond(ctxt, frame);
         break;
-    /* case MAPLE_CMD_NOP: */
+    case MAPLE_CMD_BWRITE:
+        maple_handle_bwrite(ctxt, frame);
+        break;
+    case MAPLE_CMD_SETCOND:
+        maple_handle_setcond(ctxt, frame);
+        break;
+        /* case MAPLE_CMD_NOP: */
     /*     frame->output_len = 0; */
     /*     maple_write_frame_resp(frame, MAPLE_RESP_NONE); */
     /*     break; */
@@ -155,13 +167,14 @@ maple_handle_devinfo(struct maple *ctxt, struct maple_frame *frame) {
     if (dev->enable) {
         struct maple_devinfo devinfo;
         maple_device_info(dev, &devinfo);
+
         maple_compile_devinfo(&devinfo, frame->output_data);
         frame->output_len = MAPLE_DEVINFO_SIZE;
-        maple_write_frame_resp(frame, MAPLE_RESP_DEVINFO);
+        maple_write_frame_resp(ctxt, frame, MAPLE_RESP_DEVINFO);
     } else {
         // this port/unit combo is not plugged in
         frame->output_len = 0;
-        maple_write_frame_resp(frame, MAPLE_RESP_NONE);
+        maple_write_frame_resp(ctxt, frame, MAPLE_RESP_NONE);
     }
 
     maple_dma_complete(ctxt);
@@ -186,7 +199,7 @@ maple_handle_getcond(struct maple *ctxt, struct maple_frame *frame) {
             frame->output_len = MAPLE_KEYBOARD_COND_SIZE;
             break;
         }
-        maple_write_frame_resp(frame, MAPLE_RESP_DATATRF);
+        maple_write_frame_resp(ctxt, frame, MAPLE_RESP_DATATRF);
     } else {
         error_set_feature("proper response for when the guest tries to send "
                           "the GETCOND command to an empty maple port");
@@ -196,11 +209,114 @@ maple_handle_getcond(struct maple *ctxt, struct maple_frame *frame) {
     maple_dma_complete(ctxt);
 }
 
-void maple_write_frame_resp(struct maple_frame *frame, unsigned resp_code) {
+/*
+ * Write data to a maple device
+ *
+ * PuruPuru uses this, VMU apparently does too
+ *
+ * First DWORD should be the function of the device that receives the command
+ * (like PuruPuru, LCD, memory, etc).
+ *
+ * Other data appears to be specific to each function, although the second
+ * DWORD is always 0?
+ */
+static void maple_handle_bwrite(struct maple *ctxt, struct maple_frame *frame) {
+    MAPLE_TRACE("BWRITE maplebus frame received\n");
+
+    struct maple_device *dev = maple_device_get(ctxt, frame->maple_addr);
+
+    if (dev->enable) {
+        struct maple_bwrite bwrite;
+
+        bwrite.n_dwords = frame->input_len / sizeof(uint32_t);
+        size_t n_bytes = sizeof(uint32_t) * bwrite.n_dwords;
+        if (n_bytes > MAPLE_FRAME_INPUT_DATA_LEN) {
+            /*
+             * too much data, don't want to over-read the buffer in struct
+             * maple_frame!
+             */
+            RAISE_ERROR(ERROR_UNIMPLEMENTED);
+        }
+
+        bwrite.dat = malloc(n_bytes);
+        if (!bwrite.dat)
+            RAISE_ERROR(ERROR_FAILED_ALLOC);
+        memcpy(bwrite.dat, frame->input_data, n_bytes);
+
+        maple_device_bwrite(dev, &bwrite);
+
+        maple_write_frame_resp(ctxt, frame, MAPLE_RESP_DATATRF);
+
+        free(bwrite.dat);
+    } else {
+        error_set_feature("proper response for when the guest tries to send "
+                          "the BWRITE command to an empty maple port");
+        RAISE_ERROR(ERROR_UNIMPLEMENTED);
+    }
+
+    maple_dma_complete(ctxt);
+}
+
+/*
+ * AFAIK VMU uses this to beep and PuruPuru uses this to vibrate.
+ *
+ * TBH i feel like this and bwrite are redundandt but that's the way things are.
+ */
+static void
+maple_handle_setcond(struct maple *ctxt, struct maple_frame *frame) {
+    MAPLE_TRACE("SETCOND maplebus frame received\n");
+
+    struct maple_device *dev = maple_device_get(ctxt, frame->maple_addr);
+
+    if (dev->enable) {
+        struct maple_setcond setcond;
+
+        setcond.n_dwords = frame->input_len / sizeof(uint32_t);
+        size_t n_bytes = sizeof(uint32_t) * setcond.n_dwords;
+        if (n_bytes > MAPLE_FRAME_INPUT_DATA_LEN) {
+            /*
+             * too much data, don't want to over-read the buffer in struct
+             * maple_frame!
+             */
+            RAISE_ERROR(ERROR_UNIMPLEMENTED);
+        }
+
+        setcond.dat = malloc(n_bytes);
+        if (!setcond.dat)
+            RAISE_ERROR(ERROR_FAILED_ALLOC);
+        memcpy(setcond.dat, frame->input_data, n_bytes);
+
+        maple_device_setcond(dev, &setcond);
+
+        maple_write_frame_resp(ctxt, frame, MAPLE_RESP_DATATRF);
+
+        free(setcond.dat);
+    } else {
+        error_set_feature("proper response for when the guest tries to send "
+                          "the BWRITE command to an empty maple port");
+        RAISE_ERROR(ERROR_UNIMPLEMENTED);
+    }
+
+    maple_dma_complete(ctxt);
+}
+
+static void maple_write_frame_resp(struct maple *ctxt,
+                            struct maple_frame *frame, unsigned resp_code) {
+    unsigned subdevs = 0;
+    unsigned port, unit;
+    maple_addr_unpack(frame->maple_addr, &port, &unit);
+    if (unit == 0) {
+        if (ctxt->devs[port][1].enable)
+            subdevs |= 1;
+        if (ctxt->devs[port][2].enable)
+            subdevs |= 2;
+    }
+
     unsigned len = frame->output_len / 4;
     uint32_t pkt_hdr = ((resp_code << MAPLE_CMD_SHIFT) & MAPLE_CMD_MASK) |
         ((frame->maple_addr << MAPLE_ADDR_SHIFT) & MAPLE_ADDR_MASK) |
-        ((len << MAPLE_PACK_LEN_SHIFT) & MAPLE_PACK_LEN_MASK);
+        ((len << MAPLE_PACK_LEN_SHIFT) & MAPLE_PACK_LEN_MASK) |
+        (subdevs << MAPLE_PORT_SHIFT);
 
     sh4_dmac_transfer_to_mem(dreamcast_get_cpu(), frame->recv_addr,
                              sizeof(pkt_hdr), 1, &pkt_hdr);

src/libwashdc/CMakeLists.txt
@@ -144,6 +144,8 @@ set(libwashdc_sources "${WASHDC_SOURCE_DIR}/hw/sh4/sh4.c"
                       "${WASHDC_SOURCE_DIR}/hw/maple/maple_controller.c"
                       "${WASHDC_SOURCE_DIR}/hw/maple/maple_keyboard.h"
                       "${WASHDC_SOURCE_DIR}/hw/maple/maple_keyboard.c"
+                      "${WASHDC_SOURCE_DIR}/hw/maple/maple_purupuru.h"
+                      "${WASHDC_SOURCE_DIR}/hw/maple/maple_purupuru.c"
                       "${WASHDC_SOURCE_DIR}/hw/maple/maple_reg.h"
                       "${WASHDC_SOURCE_DIR}/hw/maple/maple_reg.c"
                       "${WASHDC_SOURCE_DIR}/hw/aica/aica_rtc.h"

src/libwashdc/dreamcast.c
@@ -650,14 +650,32 @@ dreamcast_init(char const *gdi_path,
     maple_init(&maple, &sh4_clock);
 
     unsigned port;
-    for (port = 0; port < 4; port++)
-        if (strcmp(controllers[port][0], "dreamcast_controller") == 0) {
-            maple_device_init(&maple, maple_addr_pack(port, 0),
-                              MAPLE_DEVICE_CONTROLLER);
-        } else if (strcmp(controllers[port][0], "dreamcast_keyboard_us") == 0) {
-            maple_device_init(&maple, maple_addr_pack(port, 0),
-                              MAPLE_DEVICE_KEYBOARD);
+    for (port = 0; port < 4; port++) {
+        char const *unit0 = controllers[port][0];
+        if (unit0) {
+            if (strcmp(unit0, "dreamcast_controller") == 0) {
+                maple_device_init(&maple, maple_addr_pack(port, 0),
+                                  MAPLE_DEVICE_CONTROLLER);
+            } else if (strcmp(unit0, "dreamcast_keyboard_us") == 0) {
+                maple_device_init(&maple, maple_addr_pack(port, 0),
+                                  MAPLE_DEVICE_KEYBOARD);
+            }
         }
+
+        /*
+         * it is theoretically possible to have a VMU, PuruPuru, etc plugged
+         * into unit 0, but there is no way to do that without hacking up your
+         * own custom hardware so it's not allowed in this emulator.
+         */
+        unsigned unit;
+        for (unit = 1; unit <= 2; unit++) {
+            if (controllers[port][unit] &&
+                strcmp(controllers[port][unit], "purupuru") == 0) {
+                maple_device_init(&maple, maple_addr_pack(port, unit),
+                                  MAPLE_DEVICE_PURUPURU);
+            }
+        }
+    }
 
     // hook up the irl line
     sh4_register_irl_line(&cpu, holly_intc_irl_line_fn, NULL);

src/libwashdc/hw/maple/maple.h
@@ -57,7 +57,9 @@ enum maple_cmd {
     MAPLE_CMD_FUCK = 0,
     MAPLE_CMD_DEVINFO = 1,
     MAPLE_CMD_NOP = 7,
-    MAPLE_CMD_GETCOND = 9
+    MAPLE_CMD_GETCOND = 9,
+    MAPLE_CMD_BWRITE = 12,
+    MAPLE_CMD_SETCOND = 14
 };
 
 #define MAPLE_FRAME_OUTPUT_DATA_LEN 1024
@@ -108,8 +110,6 @@ struct maple {
     uint8_t reg_backing[N_MAPLE_REGS];
     uint32_t reg_msys;
 };
-
-void maple_write_frame_resp(struct maple_frame *frame, unsigned resp_code);
 
 #define MAPLE_TRACE(msg, ...) LOG_DBG("MAPLE: "msg, ##__VA_ARGS__)
 

src/libwashdc/hw/maple/maple_device.c
@@ -67,6 +67,9 @@ int maple_device_init(struct maple *maple, unsigned maple_addr,
     case MAPLE_DEVICE_KEYBOARD:
         dev->sw = &maple_keyboard_switch_table;
         break;
+    case MAPLE_DEVICE_PURUPURU:
+        dev->sw = &maple_purupuru_switch_table;
+        break;
     default:
         RAISE_ERROR(ERROR_INTEGRITY);
     }
@@ -93,8 +96,8 @@ void maple_device_info(struct maple_device *dev,
     if (dev->sw->dev_info) {
         dev->sw->dev_info(dev, devinfo);
     } else {
-        MAPLE_TRACE("no dev_info implementation for %s!?\n",
-                    dev->sw->device_type);
+        LOG_ERROR("no dev_info implementation for %s!?\n",
+                  dev->sw->device_type);
         memset(devinfo, 0, sizeof(*devinfo));
     }
 }
@@ -103,9 +106,26 @@ void maple_device_cond(struct maple_device *dev, struct maple_cond *cond) {
     if (dev->sw->dev_get_cond) {
         dev->sw->dev_get_cond(dev, cond);
     } else {
-        MAPLE_TRACE("no get_cond implementation for %s!?\n",
-                    dev->sw->device_type);
+        LOG_ERROR("no get_cond implementation for %s!?\n",
+                  dev->sw->device_type);
         memset(cond, 0, sizeof(*cond));
+    }
+}
+
+void maple_device_bwrite(struct maple_device *dev, struct maple_bwrite *bwrite) {
+    if (dev->sw->dev_bwrite) {
+        dev->sw->dev_bwrite(dev, bwrite);
+    } else {
+        LOG_ERROR("no bwrite implementation for %s!?\n", dev->sw->device_type);
+    }
+}
+
+void maple_device_setcond(struct maple_device *dev, struct maple_setcond *cond) {
+    if (dev->sw->dev_set_cond) {
+        dev->sw->dev_set_cond(dev, cond);
+    } else {
+        LOG_ERROR("no set_cond implementation for %s!?\n",
+                  dev->sw->device_type);
     }
 }
 

src/libwashdc/hw/maple/maple_device.h
@@ -39,12 +39,14 @@
 
 #include "maple_controller.h"
 #include "maple_keyboard.h"
+#include "maple_purupuru.h"
 
 struct maple_device;
 struct maple;
 
 #define MAPLE_FUNC_CONTROLLER 0x01000000
 #define MAPLE_FUNC_KEYBOARD   0x40000000
+#define MAPLE_FUNC_PURUPURU   0x00010000
 
 #define MAPLE_DEV_NAME_LEN 30
 #define MAPLE_DEV_LICENSE_LEN 60
@@ -115,6 +117,16 @@ enum MAPLE_COND_TYPE {
     MAPLE_COND_TYPE_KEYBOARD
 };
 
+struct maple_bwrite {
+    unsigned n_dwords;
+    uint32_t *dat;
+};
+
+struct maple_setcond {
+    unsigned n_dwords;
+    uint32_t *dat;
+};
+
 // controller state (response to MAPLE_CMD_GETCOND)
 struct maple_cond {
     enum MAPLE_COND_TYPE tp;
@@ -138,16 +150,22 @@ struct maple_switch_table {
     void (*dev_info)(struct maple_device*, struct maple_devinfo*);
 
     void (*dev_get_cond)(struct maple_device*, struct maple_cond*);
+
+    void (*dev_bwrite)(struct maple_device*, struct maple_bwrite*);
+
+    void (*dev_set_cond)(struct maple_device*, struct maple_setcond*);
 };
 
 enum maple_device_type {
     MAPLE_DEVICE_CONTROLLER,
-    MAPLE_DEVICE_KEYBOARD
+    MAPLE_DEVICE_KEYBOARD,
+    MAPLE_DEVICE_PURUPURU
 };
 
 union maple_device_ctxt {
     struct maple_controller cont;
     struct maple_keyboard kbd;
+    struct maple_purupuru purupuru;
 };
 
 struct maple_device {
@@ -170,6 +188,10 @@ void maple_device_cleanup(struct maple *maple, unsigned addr);
 void maple_device_info(struct maple_device *dev, struct maple_devinfo *devinfo);
 
 void maple_device_cond(struct maple_device *dev, struct maple_cond *cond);
+
+void maple_device_bwrite(struct maple_device *dev, struct maple_bwrite *bwrite);
+
+void maple_device_setcond(struct maple_device *dev, struct maple_setcond *cond);
 
 extern struct maple_switch_table maple_controller_switch_table;
 

src/libwashdc/hw/maple/maple_purupuru.c
@@ -1,0 +1,177 @@
+/*******************************************************************************
+ *
+ * Copyright 2020 snickerbockers
+ * snickerbockers@washemu.org
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors
+ * may be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ******************************************************************************/
+
+#include <string.h>
+
+#include "maple_device.h"
+#include "washdc/error.h"
+#include "log.h"
+
+/*
+ * Puru Puru - AKA the "jump pack" (or "rumble pak" if you're a nintendrone)
+ *
+ * It was marketed as the Jump Pack in NA, but the ID string the dev info
+ * command returns calls it a Puru Puru, and that ended up being what the
+ * homebrew community calls it.  That's probably what it's called in Japanese.
+ */
+
+static int purupuru_dev_init(struct maple_device *dev);
+static void purupuru_dev_cleanup(struct maple_device *dev);
+static void purupuru_dev_info(struct maple_device *dev,
+                              struct maple_devinfo *output);
+static void purupuru_dev_get_cond(struct maple_device *dev,
+                                  struct maple_cond *cond);
+static void purupuru_dev_set_cond(struct maple_device *dev,
+                                  struct maple_setcond *cond);
+static void
+purupuru_dev_bwrite(struct maple_device *dev, struct maple_bwrite *bwrite);
+
+/*
+ * TODO: need to verify these on real hardware since I don't have access to any
+ * of my dreamcasts right now
+ *
+ * I'm very confident "Puru Puru Pack" is the correct identifier based on old
+ * logs captured from real hardware, but the license string may or may not be
+ * correct; I'm just assuming that it matches the string on Dreamcast
+ * controller.
+ */
+#define MAPLE_PURUPURU_STRING "Puru Puru Pack               "
+#define MAPLE_PURUPURU_LICENSE                                      \
+    "Produced By or Under License From SEGA ENTERPRISES,LTD.    "
+
+struct maple_switch_table maple_purupuru_switch_table = {
+    .device_type = "purupuru",
+    .dev_init = purupuru_dev_init,
+    .dev_cleanup = purupuru_dev_cleanup,
+    .dev_info = purupuru_dev_info,
+    .dev_get_cond = purupuru_dev_get_cond,
+    .dev_bwrite = purupuru_dev_bwrite,
+    .dev_set_cond = purupuru_dev_set_cond
+};
+
+static int purupuru_dev_init(struct maple_device *dev) {
+    if (!(dev->enable && (dev->tp == MAPLE_DEVICE_PURUPURU)))
+        RAISE_ERROR(ERROR_INTEGRITY);
+
+    memset(&dev->ctxt, 0, sizeof(dev->ctxt));
+
+    dev->tp = MAPLE_DEVICE_PURUPURU;
+
+    return 0;
+}
+
+static void purupuru_dev_cleanup(struct maple_device *dev) {
+    if (!(dev->enable && (dev->tp == MAPLE_DEVICE_PURUPURU)))
+        RAISE_ERROR(ERROR_INTEGRITY);
+
+    // do nothing
+}
+
+static void purupuru_dev_info(struct maple_device *dev,
+                              struct maple_devinfo *output) {
+    if (!(dev->enable && (dev->tp == MAPLE_DEVICE_PURUPURU)))
+        RAISE_ERROR(ERROR_INTEGRITY);
+
+    memset(output, 0, sizeof(*output));
+
+    output->func = MAPLE_FUNC_PURUPURU;
+
+    strncpy(output->dev_name, MAPLE_PURUPURU_STRING, MAPLE_DEV_NAME_LEN);
+    output->dev_name[MAPLE_DEV_NAME_LEN - 1] = '\0';
+    strncpy(output->license, MAPLE_PURUPURU_LICENSE, MAPLE_DEV_LICENSE_LEN);
+    output->license[MAPLE_DEV_LICENSE_LEN - 1] = '\0';
+
+    /*
+     * TODO: I have no idea what the correct values of these fields should be
+     * for Puru Puru.  I'm just copying from Dreamcast Controller!
+     */
+    output->func_data[0] = 0xfe060f00;
+    output->func_data[1] = 0x00000000;
+    output->func_data[2] = 0x724400ff;
+    output->area_code = 0xff;
+    output->dir = 0;
+    output->standby_power = 0x01ae;
+    output->max_power = 0x01f4;
+}
+
+static void
+purupuru_dev_bwrite(struct maple_device *dev, struct maple_bwrite *bwrite) {
+    LOG_INFO("%s - %u dwords\n", __func__, bwrite->n_dwords);
+    unsigned idx;
+    for (idx = 0; idx < bwrite->n_dwords; idx++) {
+        LOG_INFO("\t%08X\n", (unsigned)bwrite->dat[idx]);
+    }
+}
+
+static void purupuru_dev_set_cond(struct maple_device *dev,
+                                  struct maple_setcond *cond) {
+    if (cond->n_dwords >= 2 && (cond->dat[0] & MAPLE_FUNC_PURUPURU)) {
+        /*
+         * TODO: there's no frontend support for vibrating the controller yet
+         * so all we can do is printf that we should be vibrating.
+         *
+         * Also, I don't know how to decode the vibrate command here.  There's
+         * some meaning to cond->dat[1] that effects duration and pattern but
+         * I don't have any good docs or homebrew to go off of.  If I had access
+         * to a Dreamcast I could write some homebrew and work it out but I'm
+         * stuck on the east coast for cancer treatments and all my stuff's
+         * still in California.
+         */
+        printf("***** BZZZZZZZZZZZZZZZ %08X ****\n", (unsigned)cond->dat[1]);
+    }
+}
+
+static void purupuru_dev_get_cond(struct maple_device *dev,
+                                  struct maple_cond *cond) {
+    if (!(dev->enable && (dev->tp == MAPLE_DEVICE_CONTROLLER)))
+        RAISE_ERROR(ERROR_INTEGRITY);
+
+    RAISE_ERROR(ERROR_UNIMPLEMENTED);
+    /* struct maple_controller *cont = &dev->ctxt.cont; */
+
+    /* memset(cond, 0, sizeof(*cond)); */
+    /* cond->tp = MAPLE_COND_TYPE_CONTROLLER; */
+
+    /* cond->cont.func = MAPLE_FUNC_CONTROLLER; */
+
+    /* // invert because Dreamcast controller has active-low buttons */
+    /* cond->cont.btn = ~cont->btns; */
+
+    /* cond->cont.trig_r = cont->axes[MAPLE_CONTROLLER_AXIS_R_TRIG]; */
+    /* cond->cont.trig_l = cont->axes[MAPLE_CONTROLLER_AXIS_L_TRIG]; */
+
+    /* cond->cont.js_x = cont->axes[MAPLE_CONTROLLER_AXIS_JOY1_X]; */
+    /* cond->cont.js_y = cont->axes[MAPLE_CONTROLLER_AXIS_JOY1_Y]; */
+    /* cond->cont.js_x2 = cont->axes[MAPLE_CONTROLLER_AXIS_JOY2_X]; */
+    /* cond->cont.js_y2 = cont->axes[MAPLE_CONTROLLER_AXIS_JOY2_X]; */
+}

src/libwashdc/hw/maple/maple_purupuru.h
@@ -1,0 +1,43 @@
+/*******************************************************************************
+ *
+ * Copyright 2020 snickerbockers
+ * snickerbockers@washemu.org
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors
+ * may be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ******************************************************************************/
+
+#ifndef MAPLE_PURUPURU_H_
+#define MAPLE_PURUPURU_H_
+
+extern struct maple_switch_table maple_purupuru_switch_table;
+
+struct maple_purupuru {
+    // this page left intentionally blank
+};
+
+#endif

commit fd11a4c4c788c0f96ec84b5fd35438950b2fa562

maple: fix unitinialized response length for bwrite, setcond
this commit resolves segmentation faults introduced by
fb87e96dba12087b515c15692e27d27d6045dc5b

src/libwashdc/hw/maple/maple.c
@@ -245,6 +245,7 @@ static void maple_handle_bwrite(struct maple *ctxt, struct maple_frame *frame) {
 
         maple_device_bwrite(dev, &bwrite);
 
+        frame->output_len = 0;
         maple_write_frame_resp(ctxt, frame, MAPLE_RESP_DATATRF);
 
         free(bwrite.dat);
@@ -288,6 +289,7 @@ maple_handle_setcond(struct maple *ctxt, struct maple_frame *frame) {
 
         maple_device_setcond(dev, &setcond);
 
+        frame->output_len = 0;
         maple_write_frame_resp(ctxt, frame, MAPLE_RESP_DATATRF);
 
         free(setcond.dat);