washingtondc-emu/washingtondc

commit 6f4c63abc6a929c9e8f6e9607d69b16942809a1f

deep_syscall_trace: fetch the GD-ROM vector from memory instead of hardcoding it
this is way less efficient but also way more accurate since the vector can be
overwritten.

src/libwashdc/dreamcast.c
@@ -84,6 +84,7 @@
 #include "sound.h"
 #include "washdc/hostfile.h"
 #include "hw/sys/holly_intc.h"
+#include "deep_syscall_trace.h"
 
 #ifdef ENABLE_TCP_SERIAL
 #include "serial_server.h"
@@ -557,6 +558,13 @@ dreamcast_init(char const *gdi_path,
     gfx_init(gfx_if);
     dc_sound_init(snd_intf);
 
+    memory_map_init(&mem_map);
+    memory_map_init(&arm7_mem_map);
+
+#ifdef DEEP_SYSCALL_TRACE
+    deep_syscall_trace_init(&mem_map);
+#endif
+
     memory_init(&dc_mem);
     flash_mem_init(&flash_mem, config_get_dc_flash_path(), flash_mem_writeable);
     boot_rom_init(&firmware, config_get_dc_bios_path());
@@ -670,11 +678,9 @@ dreamcast_init(char const *gdi_path,
     sh4_register_pdtra_read_handler(&cpu, on_pdtra_read);
     sh4_register_pdtra_write_handler(&cpu, on_pdtra_write);
 
-    memory_map_init(&mem_map);
     construct_sh4_mem_map(&cpu, &mem_map);
     sh4_set_mem_map(&cpu, &mem_map);
 
-    memory_map_init(&arm7_mem_map);
     construct_arm7_mem_map(&arm7_mem_map);
     arm7_set_mem_map(&arm7, &arm7_mem_map);
 
@@ -747,9 +753,6 @@ void dreamcast_cleanup() {
 
     aica_rtc_cleanup(&rtc);
 
-    memory_map_cleanup(&arm7_mem_map);
-    memory_map_cleanup(&mem_map);
-
     // disconnect PDTRA read/write handlers
     sh4_register_pdtra_read_handler(&cpu, NULL);
     sh4_register_pdtra_write_handler(&cpu, NULL);
@@ -783,6 +786,13 @@ void dreamcast_cleanup() {
     boot_rom_cleanup(&firmware);
     flash_mem_cleanup(&flash_mem);
     memory_cleanup(&dc_mem);
+
+#ifdef DEEP_SYSCALL_TRACE
+    deep_syscall_trace_cleanup();
+#endif
+
+    memory_map_cleanup(&arm7_mem_map);
+    memory_map_cleanup(&mem_map);
 
     dc_sound_cleanup();
     gfx_cleanup();

src/libwashdc/deep_syscall_trace.c
@@ -46,10 +46,30 @@
 
 #include "deep_syscall_trace.h"
 
-#define GDROM_SYSCALL_ADDR 0x8c001000
-
 static uint32_t ret_addr;
 static bool in_syscall;
+
+static struct memory_map *mem_map;
+
+void deep_syscall_trace_init(struct memory_map *map) {
+    mem_map = map;
+}
+
+void deep_syscall_trace_cleanup(void) {
+}
+
+static uint32_t syscall_gdrom_vector(void) {
+    if (!mem_map) {
+        LOG_ERROR("You forgot to call deep_syscall_trace_init, dumbass!\n");
+        return 0x8c001000; // this is the default value
+    }
+    uint32_t ret;
+    if (memory_map_try_read_32(mem_map, 0x8c0000bc, &ret) != 0) {
+        LOG_ERROR("Failure to read GD-ROM syscall vector!\n");
+        return 0x8c001000; // this is the default value
+    }
+    return ret;
+}
 
 #define SYSCALL_TRACE(msg, ...)                                         \
     do {                                                                \
@@ -126,7 +146,7 @@ static struct syscall_stat {
 
 void deep_syscall_notify_jump(addr32_t pc) {
     Sh4 *sh4 = dreamcast_get_cpu();
-    if (pc == GDROM_SYSCALL_ADDR) {
+    if (pc == syscall_gdrom_vector()) {
         if (in_syscall) {
             SYSCALL_TRACE("recursive syscall detected.  "
                           "Trace will be unreliable!\n");

src/libwashdc/deep_syscall_trace.h
@@ -2,7 +2,7 @@
  *
  *
  *    WashingtonDC Dreamcast Emulator
- *    Copyright (C) 2017, 2019 snickerbockers
+ *    Copyright (C) 2017, 2019, 2020 snickerbockers
  *
  *    This program is free software: you can redistribute it and/or modify
  *    it under the terms of the GNU General Public License as published by
@@ -28,6 +28,9 @@
 #define DEEP_SYSCALL_TRACE_H_
 
 #include "washdc/types.h"
+
+void deep_syscall_trace_init(struct memory_map *mem_map);
+void deep_syscall_trace_cleanup(void);
 
 void deep_syscall_notify_jump(addr32_t pc);
 

commit e2679a72722d67e8688992101b66f7a04da374d2

fix build breakage from 6f4c63abc6a929c9e8f6e9607d69b16942809a1f
god-dammit

src/libwashdc/dreamcast.c
@@ -84,7 +84,10 @@
 #include "sound.h"
 #include "washdc/hostfile.h"
 #include "hw/sys/holly_intc.h"
+
+#ifdef DEEP_SYSCALL_TRACE
 #include "deep_syscall_trace.h"
+#endif
 
 #ifdef ENABLE_TCP_SERIAL
 #include "serial_server.h"